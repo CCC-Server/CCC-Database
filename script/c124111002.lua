846a0e77936a3712cebb3bc2c096ee81d1680adc
Crowel <faithtaeho@naver.com>
2024-03-13 16:45:56 +0900

    에데이아님 카드 구현
    - 선혈의 조형사-드레이퓨스

--- script/c124111002.lua
+++ script/c124111002.lua
@@ -1,6 +1,90 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--선혈의 조형사-드레이퓨스
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--xyz summon
+	Xyz.AddProcedure(c,nil,6,2)
+	c:EnableReviveLimit()
+	--indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetCondition(s.incon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Negate an opponent's targeting effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.discon)
+	e2:SetTarget(s.distg)
+	e2:SetCost(s.discost)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	aux.DoubleSnareValidity(c,LOCATION_MZONE)
+	--Attach 2 cards to itself
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.attcon)
+	e3:SetTarget(s.atttg)
+	e3:SetOperation(s.attop)
+	c:RegisterEffect(e3)
+
+end
+function s.incon(e)
+	return e:GetHandler():GetOverlayCount()>0
+end
+function s.tfilter(c,tp)
+	return c:IsOnField() and c:IsControler(tp)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return tg and tg:IsExists(s.tfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	local rc=re:GetHandler()
+	if rc:IsDestructable() and rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function s.attcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsPreviousLocation,1,nil,LOCATION_ONFIELD)
+end
+function s.attfilter(c,e)
+	return not c:IsImmuneToEffect(e)
+end
+function s.atttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.attfilter,tp,0,LOCATION_GRAVE,2,nil,e)
+		and e:GetHandler():IsType(TYPE_XYZ) end
+end
+function s.attop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local tg=Duel.SelectMatchingCard(tp,s.attfilter,tp,0,LOCATION_GRAVE,2,2,nil,e)
+	if #tg==2 then
+		Duel.Overlay(c,tg,true)
+	end
 end
