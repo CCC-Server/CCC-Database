844eda9291368dd4d3100d809c82e0c4fe6891b9
Crowel <faithtaeho@naver.com>
2024-03-14 14:33:10 +0900

    세이하이님 카드 구현 (미완료)
    - 전설의 초전사 카오스 솔저

--- script/c124101006.lua
+++ script/c124101006.lua
@@ -1,6 +1,121 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--전설의 초전사 카오스 솔저
+Duel.LoadScript("archetype_seihai.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	c:EnableReviveLimit()
+	--Indestructible
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.indescon)
+	e1:SetOperation(s.indesop)
+	c:RegisterEffect(e1)
+	--Banish all face-up opponent cards
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.rmcost)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
+	--Extra attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DAMAGE_STEP_END)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.exacon)
+	e3:SetTarget(s.exatg)
+	e3:SetOperation(s.exaop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={124101008,14094090}
+--Indestructible
+function s.indescon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return rc:IsArchetype(ARCHETYPE_SUPER_SOLDIER) and rc:IsRitualSpell() and e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
+end
+function s.indesop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3000)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(3001)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetDescription(3061)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+end
+--Banish all face-up opponent cards
+function s.rmfilter1(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_DARK) and not c:IsCode(id)
+		and aux.SpElimFilter(c,true,false) and c:IsAbleToRemoveAsCost()
+end
+function s.attrfilter(c,sg)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and sg:FilterCount(Card.IsAttribute,c,ATTRIBUTE_DARK)==1
+end
+function s.rmrescon(sg,e,tp,mg)
+	return sg:IsExists(s.attrfilter,1,nil,sg) and aux.ChkfMMZ(1)(sg,e,tp,mg)
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.rmfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rmrescon,0) end
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rmrescon,1,tp,HINTMSG_REMOVE)
+	Duel.Remove(sg,POS_FACEUP,REASON_COST)
+end
+function s.rmfilter2(c)
+	return c:IsAbleToRemove() and c:IsFaceup()
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter2,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(s.rmfilter2,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,LOCATION_MZONE,1-tp)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.rmfilter2,tp,0,LOCATION_ONFIELD,nil)
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+--Extra attack
+function s.exacon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker()==e:GetHandler()
+end
+--
+function s.exafilter(c)
+	return c:IsSetCard(SET_BLACK_LUSTER_SOLDIER) and c:IsFaceup()
+end
+function s.exatg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.exafilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.exaop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.exafilter,tp,LOCATION_MZONE,0,nil)
+	g:ForEach(function(tc)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetValue(1)
+		e1:SetCondition(s.atkcon)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetAttackAnnouncedCount()>0
 end
