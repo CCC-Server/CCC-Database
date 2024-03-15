f7642784532e6a17bffd838960b130b7e5be65e9
Crowel <faithtaeho@naver.com>
2024-03-15 19:24:52 +0900

    세이하이님 카드 구현 (미완료)
    - 초전사 크리보
    - 암흑 초전사 가이아

--- script/c124101001.lua
+++ script/c124101001.lua
@@ -1,6 +1,89 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--초전사 크리보
+Duel.LoadScript("archetype_seihai.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--Replace destruction
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.reptg)
+	e1:SetValue(s.repval)
+	e1:SetOperation(s.repop)
+	c:RegisterEffect(e1)
+	--Can be used for a Ritual Summon while in the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_EXTRA_RITUAL_MATERIAL)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.mtcon)
+	e2:SetValue(s.mtval)
+	c:RegisterEffect(e2)
+	--Search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_BE_MATERIAL)
+	e4:SetCondition(s.thcon)
+	c:RegisterEffect(e4)
+end
+s.listed_names={id}
+s.listed_series={SET_CHAOS,SET_BLACK_LUSTER_SOLDIER,ARCHETYPE_SUPER_SOLDIER}
+--Replace destruction
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsSetCard(SET_BLACK_LUSTER_SOLDIER)
+		and c:IsReason(REASON_BATTLE|REASON_EFFECT)
+		and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT|REASON_REPLACE)
+end
+--Can be used for a Ritual Summon while in the GY
+function s.mtcon(e)
+	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),CARD_SPIRIT_ELIMINATION)
+end
+function s.mtval(e,c)
+	return c:IsSetCard(SET_BLACK_LUSTER_SOLDIER)
+end
+--Search
+function s.thfilter(c)
+	return not c:IsCode(id)
+		and (c:IsArchetype(ARCHETYPE_SUPER_SOLDIER) or (c:IsSetCard(SET_BLACK_LUSTER_SOLDIER) and c:IsMonster()))
+		and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_RITUAL
+		and e:GetHandler():GetReasonCard():IsSetCard(SET_BLACK_LUSTER_SOLDIER)
 end
--- script/c124101004.lua
+++ script/c124101004.lua
@@ -1,6 +1,77 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--암흑 초전사 가이아
+Duel.LoadScript("archetype_seihai.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--Special summon procedure
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,function(re) return not re:IsMonsterEffect() end)
+	--Set a card and Special summon a token
+	local e2=Effect.CreateEffect(c)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+s.listed_names={id+1}
+s.listed_series={SET_GAIA_THE_FIERCE_KNIGHT,ARCHETYPE_SUPER_SOLDIER}
+--Special summon procedure
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>0
+end
+--Set a card and Special summon a token
+function s.stfilter(c)
+	return c:IsRitualSpell() and c:IsArchetype(ARCHETYPE_SUPER_SOLDIER)
+		and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,1,RACE_WARRIOR,ATTRIBUTE_EARTH) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_DECK,0,1,nil) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local sg=Duel.SelectMatchingCard(tp,s.stfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if Duel.SSet(tp,sg)>0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,1,RACE_WARRIOR,ATTRIBUTE_EARTH) then
+			local token=Duel.CreateToken(tp,id+1)
+			if Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)>0 then
+				--Cannot activate effect of monsters
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetDescription(aux.Stringid(id,2))
+				e1:SetType(EFFECT_TYPE_FIELD)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+				e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+				e1:SetRange(LOCATION_MZONE)
+				e1:SetTargetRange(1,0)
+				e1:SetValue(function(_,re) return not s.actfilter(re) end)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_CONTROL)
+				token:RegisterEffect(e1)
+			end
+	end
+end
+function s.actfilter(re)
+	local rc=re:GetHandler()
+	return not (re:IsMonsterEffect() and not rc:IsRitualMonster() and not rc:IsSummonableCard())
 end
