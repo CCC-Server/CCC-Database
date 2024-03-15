096623442c1f87a5c9ce1fc1704f3bb24bbc5352
Crowel <faithtaeho@naver.com>
2024-03-15 23:31:22 +0900

    세이하이님 카드 조정 및 구현
    - 초전사의 영당
    ※ 구현 과정 중, "의식 소환을 실행하는 효과" 판별에 문제가 생겨서 텍스트를 조정 후 구현

--- script/c124101009.lua
+++ script/c124101009.lua
@@ -1,6 +1,96 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--초전사의 영당
+Duel.LoadScript("archetype_seihai.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Cannot negate the activation
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	--Cannot negate the activated effect
+	local e3=e2:Clone
+	e3:SetCode(EFFECT_CANNOT_DISEFFECT)
+	c:RegisterEffect(e3)
+	--Reveal a card and search an appropriate card
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCost(s.thcost1)
+	e4:SetTarget(s.thtg1)
+	e4:SetOperation(s.thop1)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCost(s.thcost2)
+	e5:SetTarget(s.thtg2)
+	e5:SetOperation(s.thop2)
+	c:RegisterEffect(e5)
+end
+s.listed_series={SET_BLACK_LUSTER_SOLDIER,ARCHETYPE_SUPER_SOLDIER}
+--Cannot negate the activation and the activated effect
+function s.efilter(e,ct)
+	local p=e:GetHandler():GetControler()
+	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+	return p==tp and te:GetHandler():IsSetCard(SET_BLACK_LUSTER_SOLDIER)
+end
+--Search
+function s.cfilter1(c)
+	return c:IsSetCard(SET_BLACK_LUSTER_SOLDIER) and c:IsMonster() and not c:IsPublic()
+end
+function s.thcost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter1,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+end
+function s.thfilter1(c)
+	return c:IsArchetype(ARCHETYPE_SUPER_SOLDIER) and c:IsRitualSpell() and c:IsAbleToHand()
+end
+function s.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.cfilter2(c)
+	return c:IsArchetype(ARCHETYPE_SUPER_SOLDIER) and not c:IsPublic()
+end
+function s.thcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter2,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+end
+function s.thfilter2(c)
+	return c:IsSetCard(SET_BLACK_LUSTER_SOLDIER) and c:IsRitualMonster() and c:IsAbleToHand()
+end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
