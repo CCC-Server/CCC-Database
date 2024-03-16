1f4a9195a5f6162a6b21e35f7a13ef81bd033d76
Crowel <faithtaeho@naver.com>
2024-03-16 23:12:58 +0900

    세이하이님 카드 밸런스 조정
    초전사의 빛
    - 1턴에 1장만 발동 가능
    - 묘지에서 제외하고 서치 + 조건부 드로우하는 효과 삭제
    
    카오스 블레이드 (미구현)
    - 카드 삭제

--- script/c124101008.lua
+++ script/c124101008.lua
@@ -3,7 +3,7 @@ Duel.LoadScript("archetype_seihai.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	Ritual.AddProc({
+	local e1=Ritual.CreateProc({
 		handler=c,
 		lvtype=RITPROC_EQUAL,
 		filter=aux.FilterBoolFunction(Card.IsSetCard,SET_BLACK_LUSTER_SOLDIER),
@@ -14,20 +14,9 @@ function s.initial_effect(c)
 		forcedselection=s.ritcheck,
 		extratg=s.extratg
 	})
-	--Search
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_GRAVE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(aux.bfgcost)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 end
-s.listed_names={CARD_BLACK_LUSTER_SOLDIER}
 s.listed_series={SET_GAIA_THE_FIERCE_KNIGHT,SET_BLACK_LUSTER_SOLDIER,ARCHETYPE_SUPER_SOLDIER}
 --Activate
 function s.extragroup(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -48,38 +37,4 @@ function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.ritcheck(e,tp,g,sc)
 	return g:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
-end
---Search
-function s.thfilter1(c)
-	return c:IsArchetype(ARCHETYPE_SUPER_SOLDIER) and c:IsRitualSpell() and c:IsAbleToHand()
-end
-function s.thfilter2(c)
-	return c:IsSetCard(SET_BLACK_LUSTER_SOLDIER) and c:IsRitualMonster() and c:IsAbleToHand()
-end
-function s.thfilter(c)
-	return s.thfilter1(c) or s.thfilter2(c)
-end
-function s.rescon(sg,e,tp,mg)
-	return sg:FilterCount(s.thfilter1,nil)==1
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if chk==0 then return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g<2 then return end
-	local thg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_ATOHAND)
-	if #thg==2 then
-		Duel.SendtoHand(thg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,thg)
-		local hg=thg:Filter(Card.IsLocation,nil,LOCATION_HAND)
-		if #hg>0 then Duel.ShuffleDeck(tp) end
-		if hg:FilterCount(Card.IsCode,nil,CARD_BLACK_LUSTER_SOLDIER)>0
-			and Duel.IsPlayerCanDraw(tp,1) then
-				Duel.BreakEffect()
-				Duel.Draw(tp,1,REASON_EFFECT)
-		end
-	end
 end
