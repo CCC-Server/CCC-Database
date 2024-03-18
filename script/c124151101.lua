a3e7aa6c1243d365f5640dc340d56556c7ee9a36
Enn Kim <nominate17@gmail.com>
2024-03-19 01:07:16 +0900

    솜꽃님 카드 추가
    - "홀로우그램" 카드군
    - 홀로우그램의 금서 (스크립트)
    - 나머지 카드의 스크립트 및 이미지 구현 필요

---
+++ script/c124151101.lua
@@ -0,0 +1,55 @@
+--홀로우그램의 금서
+local s,id=GetID()
+function s.initial_effect(c)
+	--덱에서 "홀로우그램" 마법 카드를 패에 넣는다
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.selfreleasecost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--자신 묘지의 "홀로우그램" 마법 카드 1장을 세트하고 이 카드를 묘지로 되돌린다
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.settgtg)
+	e2:SetOperation(s.settgop)
+	c:RegisterEffect(e2)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0xf60) and c:IsSpell() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.settgfilter(c)
+	return c:IsSetCard(0xf60) and c:IsSpell() and c:IsSSetable()
+end
+function s.settgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.settgfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.settgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SSet(tp,g) and c:IsLocation(LOCATION_REMOVE)
+		and c:IsRelateToEffect(e) then
+		Duel.SendtoGrave(c,REASON_EFFECT+REASON_RETURN)
+	end
+end
