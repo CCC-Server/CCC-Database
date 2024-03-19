aa0a0ec360862651cc743b75a0fd855a58305de5
lintbloom <nominate17@gmail.com>
2024-03-19 21:47:31 +0900

    솜꽃님 카드 버그 수정 - 홀로우그램의 공상 (계속)

--- script/c124151101.lua
+++ script/c124151101.lua
@@ -49,7 +49,7 @@ function s.settgop(e,tp,eg,ep,ev,re,r,rp)
 	local c = e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 	local g=Duel.SelectMatchingCard(tp,s.settgfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 and Duel.SSet(tp,g) and c:IsLocation(LOCATION_REMOVE)
+	if #g>0 and Duel.SSet(tp,g) and c:IsLocation(LOCATION_REMOVED)
 		and c:IsRelateToEffect(e) then
 		Duel.SendtoGrave(c,REASON_EFFECT+REASON_RETURN)
 	end
