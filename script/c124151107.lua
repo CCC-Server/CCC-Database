56bf193f7fe559bc60b50aff5fc31d92657761a8
lintbloom <nominate17@gmail.com>
2024-03-20 16:56:27 +0900

    솜꽃님 카드 구현
    - 홀로우그램의 원령 니세
    - 그 외 텍스트 오타 수정

--- script/c124151107.lua
+++ script/c124151107.lua
@@ -1,6 +1,97 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--홀로우그램의 원령 니세
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--특수 소환
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--파괴 대체
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	c:RegisterEffect(e2)
+	--묘지로 되돌리고 서치
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOGRAVE|CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={id}
+s.listed_series={0xf60}
+function s.spfilter(c,tp)
+	return c:IsSetCard(0xf60) and c:IsAbleToRemove() and Duel.GetMZoneCount(tp,c)>0
+		and (c:IsLocation(LOCATION_HAND) or aux.SpElimFilter(c,true,true))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,c,tp)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_HAND|LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,1,c,tp)
+	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
+end
+function s.rmfilter(c)
+	return c:IsSetCard(0xf60) and c:IsAbleToRemove()
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=eg:FilterCount(s.repfilter,nil,tp)
+	local loc=LOCATION_HAND|LOCATION_ONFIELD|LOCATION_GRAVE
+	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(s.rmfilter,tp,loc,0,ct,nil) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,loc,0,ct,ct,nil)
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.tgfilter(c,e,tp)
+	return c:IsSetCard(0xf60) and c:IsAbleToGrave()
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c)
+end
+function s.thfilter(c,e,tp,tgc)
+	return c:IsSetCard(0xf60) and not c:IsOriginalCode(tgc:GetOriginalCode()) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local c=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp):GetFirst()
+	if c and Duel.SendtoGrave(c,REASON_EFFECT|REASON_RETURN) and c:IsLocation(LOCATION_GRAVE) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,c)
+		if sc then
+			Duel.SendtoHand(sc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sc)
+		end
+	end
 end
