79e151108f3977689c57e9ea1113e5e92de0ec8c
ele093 <50264650+ele093@users.noreply.github.com>
2024-03-21 02:27:07 +0900

    에데이아님 카드 스크립트 일부 구현
    쌍권총&전기톱 123 효과 / 절찬상영중 1번 효과

--- script/c124111005.lua
+++ script/c124111005.lua
@@ -1,6 +1,50 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--영원한 후일담의 전기톱
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--cannot attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e1)
+    --self destory
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_SELF_DESTROY)
+	e2:SetCondition(s.descon)
+	c:RegisterEffect(e2)
+    --destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.destg)
+	e3:SetValue(s.desval)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_NORMAL) and c:IsRace(RACE_ZOMBIE)
+end
+function s.descon(e)
+	return not Duel.IsExistingMatchingCard(s.filter,e:GetHandler():GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function s.dfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsType(TYPE_NORMAL) and c:IsRace(RACE_ZOMBIE)
+		and not c:IsReason(REASON_REPLACE) and c:IsControler(tp)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not eg:IsContains(e:GetHandler())
+		and eg:IsExists(s.dfilter,1,nil,tp) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		return true
+	else return false end
+end
+function s.desval(e,c)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsType(TYPE_NORMAL) and c:IsRace(RACE_ZOMBIE)
+		and not c:IsReason(REASON_REPLACE) and c:IsControler(e:GetHandlerPlayer())
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
 end
