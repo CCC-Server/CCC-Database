a6d5c8b145c51ebb94add4b7bb4ea7aab3aa2c9b
ele093 <50264650+ele093@users.noreply.github.com>
2024-03-21 02:27:07 +0900

    에데이아님 카드 스크립트 일부 구현
    쌍권총&전기톱 123 효과 / 절찬상영중 1번 효과

--- script/c124111009.lua
+++ script/c124111009.lua
@@ -1,6 +1,32 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--〈영원한 후일담〉 절찬 상영 중
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Normal Summon
+	--Special Summon
+end
+s.listed_names={CARD_RA,id,10000090}
+function s.thfilter(c)
+	return c:IsSetCard(0x1f40) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
