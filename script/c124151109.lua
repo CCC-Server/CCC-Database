6a864239f1c90a25f2101ea351ef8187ff658790
lintbloom <nominate17@gmail.com>
2024-03-21 16:13:52 +0900

    솜꽃님 카드 구현 - 홀로우그램의 저자 지크프로이트 - 홀로우그램의 거상 카를마: 특수 소환 효과에 명칭제약이 없던 문제 수정 (이 턴 같은 카드를 묘지로 되돌릴 수 없도록 하는 공통 제약은 구현 예정)

--- script/c124151109.lua
+++ script/c124151109.lua
@@ -1,6 +1,75 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--홀로우그램의 저자 지크프로이트
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--특수 소환
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--파괴 시 덱으로 되돌린다
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,0xff)
+	e2:SetValue(LOCATION_DECKSHF)
+	e2:SetTarget(s.tdtg)
+	c:RegisterEffect(e2)
+	--묘지로 되돌리고 파괴
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOGRAVE|CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+function s.spfilter(c,tp)
+	return c:IsSetCard(0xf60) and c:IsAbleToRemove() and Duel.GetMZoneCount(tp,c)>0
+		and (not c:IsLocation(LOCATION_GRAVE) or aux.SpElimFilter(c,true,true))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_ONFIELD|LOCATION_GRAVE,0,3,c,tp)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,3,tp,LOCATION_ONFIELD|LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_ONFIELD|LOCATION_GRAVE,0,3,3,c,tp)
+	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.tdtg(e,c)
+	local tp=e:GetHandlerPlayer()
+	return c:GetOwner()~=tp and Duel.IsPlayerCanSendtoDeck(tp,c)
+		and c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
+end
+function s.tgfilter(c,e,tp)
+	return c:IsSetCard(0xf60) and c:IsAbleToGrave()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local gg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	if not Duel.SendtoGrave(gg,REASON_EFFECT|REASON_RETURN) then return end
+	local dg=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		Duel.HintSelection(dg,true)
+		Duel.Destroy(dg,REASON_EFFECT)
+	end
 end
