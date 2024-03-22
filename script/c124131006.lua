70e206541c73f2a1694b6609abe8ef12d3789d83
Crowel <faithtaeho@naver.com>
2024-03-23 01:43:27 +0900

    서나래님 카드 스크립트 수정
    데 리퍼 ADR-01 쥬리
    - c:EnableUnsummonable()

--- script/c124131006.lua
+++ script/c124131006.lua
@@ -1,7 +1,7 @@
 --데 리퍼 ADR-01 쥬리
 local s,id=GetID()
 function s.initial_effect(c)
-c:EnableReviveLimit()
+	c:EnableUnsummonable()
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -20,7 +20,7 @@ c:EnableReviveLimit()
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
-    	--immune
+    --immune
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -32,18 +32,19 @@ c:EnableReviveLimit()
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_COIN+CATEGORY_DRAW)
+	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,id)
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.descon)
- e3:SetTarget(s.destg)
- e3:SetOperation(s.desop)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
 end
 function s.splimit(e,se,sp,st)
 	return se:GetHandler():IsSetCard(0x810)
+		and (se:IsHasType(EFFECT_TYPE_ACTIONS) or se:GetCode()==EFFECT_SPSUMMON_PROC)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -51,7 +52,7 @@ function s.spcon(e,c)
 	local rg=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,e:GetHandler())
 	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0,c)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
 	local rg=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,e:GetHandler())
 	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_DISCARD,nil,nil,true)
@@ -75,16 +76,15 @@ function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
- if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
- if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
- local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
- Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,HINTMSG_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
- local tc=Duel.GetFirstTarget()
- if tc and tc:IsRelateToEffect(e) then
-  Duel.SendtoGrave(tc,REASON_EFFECT)
-  tc:IsLocation(LOCATION_GRAVE)
- end
+	local tg=Duel.GetTargetCards()
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
+	end
 end
