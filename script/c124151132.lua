7e5643440ee517738d496d9ae2cf712deaf68a39
lintbloom <nominate17@gmail.com>
2024-03-24 01:29:56 +0900

    솜꽃님 카드 구현
    - 홀로우그램의 허상 (+효과 조정)
    - 홀로우그램의 공상: 제외되지 않은 턴에도 무효 효과를 발동할 수 있던 문제 수정

--- script/c124151132.lua
+++ script/c124151132.lua
@@ -1,6 +1,71 @@
---미구현 카드
+--홀로우그램의 허상
 Duel.LoadScript("strings.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--발동 시 효과
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMING_BATTLE_START+TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--이 턴 이 카드가 제외되어있을 경우
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetOperation(s.regop)
+	c:RegisterEffect(e2)
+	--필드의 "홀로우그램" 카드를 릴리스 불가, 제외 불가
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_RELEASE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCondition(function(e) return e:GetLabelObject():HasFlagEffect(id) end)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xf60))
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetTargetRange(LOCATION_ONFIELD,0)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4.SetCode(EFFECT_CANNOT_REMOVE)
+	c:RegisterEffect(e4)
+end
+s.listed_names={id}
+s.listed_sets={0xf60}
+function s.filter(c)
+	return c.IsFaceup() and c.IsSetCard(0xf60)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		--상대가 발동한 효과를 받지 않는다
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,0))
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(function(e,te) return te:IsActivated() and te:GetOwnerPlayer()~=e:GetHandlerPlayer() end)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		--공격 대상이 되지 않는다
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetDescription(aux.Stringid(id,1))
+		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,1,0)
 end
