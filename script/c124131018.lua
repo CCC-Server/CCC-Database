844cba4da99696a1896ee930b0e0c54548f60a0d
ele093 <50264650+ele093@users.noreply.github.com>
2024-03-25 02:46:59 +0900

    서나래님 카드 수정
    이블 히어로 이블리스

--- script/c124131018.lua
+++ script/c124131018.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Fusion material
-	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,FIEND),10)
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,FIEND),10)
 	--lizard check
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
