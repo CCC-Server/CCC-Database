e034792a60de9eaf8daf846dd480e76d2c601d54
ele093 <50264650+ele093@users.noreply.github.com>
2024-03-28 00:27:16 +0900

    서나래님 카드 추가
    "CNo.17 레 비아단 드래곤"

---
+++ script/c124131019.lua
@@ -0,0 +1,39 @@
+--CNo.17 레 비아단 드래곤
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),3,3,s.ovfilter,aux.Stringid(id,1))
+    --boost
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.atkcost)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.xyz_number=17
+s.listed_names={69610924}
+function s.ovfilter(c,tp,lc)
+	return c:IsFaceup() and c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,69610924)
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetValue(s.atkval(c))
+		c:RegisterEffect(e1)
+	end
+end
+function s.atkval(c)
+    return c:GetOverlayCount()*1000
+end
