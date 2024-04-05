01b3f4f991946114b1a45928f39b84a86f6fe0a8
ele093 <50264650+ele093@users.noreply.github.com>
2024-04-05 23:14:02 +0900

    서나래님 카드 완성
    "푸른 눈의 백염룡"

--- script/c124131021.lua
+++ script/c124131021.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	local e1=e2:Clone()
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	c:RegisterEffect(e1)
-    --attack twice
+	--attack twice
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -32,22 +32,21 @@ function s.initial_effect(c)
 end
 s.listed_names={124131020}
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetAttackTarget()~=nil end
-    local bc=e:GetHandler():GetBattleTarget()
-    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,bc:GetAttack())
+	if chk==0 then return Duel.GetAttackTarget()~=nil end
+	local bc=e:GetHandler():GetBattleTarget()
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,bc:GetAttack())
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    local atk=bc:GetAttack()
-    if c:IsFaceup() and c:IsRelateToEffect(e) and atk>0
-        and re:GetHandler():IsControler(1-tp) then
-        -- Update ATK
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(atk)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-        c:RegisterEffect(e1)
-    end
+	local c=e:GetHandle()
+	local bc=c:GetBattleTarge()
+	local atk=bc:GetAttac()
+	if c:IsFaceup() and c:IsRelateToEffect(e) and atk>0 then
+		-- Update ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
 end
