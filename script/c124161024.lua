13a5ab283e21988075921d878116c0e8a686f8b7
id187 <limajaka123@naver.com>
2024-04-16 19:28:12 +0900

    효과 변경

--- script/c124161024.lua
+++ script/c124161024.lua
@@ -15,11 +15,10 @@ function s.initial_effect(c)
 	--effect 2
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(function(e,c) return c~=e:GetHandler() and c:IsSetCard(0xf21) end)
-	e2:SetValue(function(e,te) return te:GetOwnerPlayer()==1-e:GetHandlerPlayer() and te:IsActivated() and te:IsSpellEffect() end)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetValue(s.val2)
 	c:RegisterEffect(e2)
 end
 
@@ -55,4 +54,9 @@ function s.op1(e,tc,tp,sg,chk)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetLabel(e:GetLabel())
 	c:RegisterEffect(e1)
+end
+
+--effect2
+function s.val2(e,c)
+	return c:IsFaceup() and c:IsSetCard(0xf21) and c~=e:GetHandler()
 end
