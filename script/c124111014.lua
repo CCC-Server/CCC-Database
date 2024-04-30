ed6c7c3157a88b58d6dcc2838da84133e9035673
etheia0 <tale123@naver.com>
2024-05-01 00:24:07 +0900

    영원한 후일담의 프로타고니스트 데이터 추가

---
+++ script/c124111014.lua
@@ -0,0 +1,37 @@
+--영원한 후일담의 프로타고니스트(테스트)
+local s,id=GetID()
+function s.initial_effect(c)
+	Gemini.AddProcedure(c)
+	--Special summon "zombie-type normal monster" or "영원한 후일담 monster"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.spcost)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(Gemini.EffectStatusCondition)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SendtoHand(e:GetHandler(),nil,REASON_COST)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x1f40) or (c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_NORMAL)) and c:IsMonster()
+end 
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,nil,e,tp)
+	if #g==0 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,aux.TRUE,1,tp,HINTMSG_SPSUMMON)
+	if #sg>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
