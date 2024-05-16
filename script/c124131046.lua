192a1b788699a16414522cf3ce836e5819a4b6ca
id187 <limajaka123@naver.com>
2024-05-17 00:37:03 +0900

    아르카나 포스 구현

---
+++ script/c124131046.lua
@@ -0,0 +1,97 @@
+--아르카나 포스 X-휠 오브 포츈
+local s,id=GetID()
+function s.initial_effect(c)
+	--"빛의 결계" 존재시 특수 소환
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--Toss a coin and apply the appropriate effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COIN)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetTarget(s.cointg)
+	e2:SetOperation(s.coinop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+end
+s.toss_coin=true
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,73206827),tp,LOCATION_FZONE,0,1,nil)
+end
+function s.cointg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+end
+function s.coinop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	s.arcanareg(c,Arcana.TossCoin(c,tp))
+end
+function s.arcanareg(c,coin)
+	--Heads: 드로우
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetTargetRange(1,0)
+	e1:SetCondition(s.rdcon1)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	--Tails: 제외
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.rdcon2)
+	e2:SetTarget(s.drtg2)
+	e2:SetOperation(s.drop2)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e2)
+	Arcana.RegisterCoinResult(c,coin)
+end
+function s.rdcon1(e,tp,eg,ep,ev,re,r,rp)
+	return Arcana.GetCoinResult(e:GetHandler())==COIN_HEADS and eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+function s.rdcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Arcana.GetCoinResult(e:GetHandler())==COIN_TAILS and eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
+end
+function s.drtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,1)
+end
+function s.drop2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetDecktopGroup(tp,1)
+	Duel.DisableShuffleCheck()
+	Duel.Remove(g,POS_FACEDOWN,REASON_EFFECT)
+end
