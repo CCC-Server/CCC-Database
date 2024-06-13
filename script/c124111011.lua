7ed0338524bdaabfc395629d98283854ec825473
etheia0 <tale123@naver.com>
2024-06-13 19:32:02 +0900

    영원한 후일담(속공) 및 영원한 후일담 촬영중의 텍스트를 개정 예정인 내용으로 변경, 영원한 후일담 오디션, 브레이크타임의 스크립트 추가

--- script/c124111011.lua
+++ script/c124111011.lua
@@ -1,6 +1,49 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--〈영원한 후일담〉 오디션
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+
+function s.cfilter(c,e,tp)
+	local lv=c:GetLevel()
+	return lv>0 and c:IsRace(RACE_ZOMBIE) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,lv)
+end
+function s.spfilter(c,e,tp,lv)
+	return c:IsLevelBelow(lv) and c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil,e,tp)
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_DISCARD)
+	Duel.SendtoGrave(sg,REASON_COST+REASON_DISCARD)
+	e:SetLabel(sg:GetFirst():GetLevel())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:GetSum(Card.GetLevel)<=e:GetLabel()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lv=e:GetLabel()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp,lv)
+	if #g==0 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,3,s.rescon,1,tp,HINTMSG_ATOHAND)
+	Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,sg)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(function(_,re) return re:IsMonsterEffect() and not re:GetHandler():IsRace(RACE_ZOMBIE) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
--- script/c124111012.lua
+++ script/c124111012.lua
@@ -1,6 +1,59 @@
---미구현 카드
-Duel.LoadScript("strings.lua")
+--〈영원한 후일담〉 브레이크타임
 local s,id=GetID()
 function s.initial_effect(c)
-	c:Unimplemented()
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+
+function s.costfilter(c)
+	return c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_NORMAL)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
+	e:SetLabelObject(g:GetFirst())
+	Duel.Release(g,REASON_COST)
+	g:GetFirst():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,0)
+end
+
+function s.targetfilter(c)
+	return c:IsAbleToGrave()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.targetfilter,tp,0,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.targetfilter,tp,0,LOCATION_ONFIELD,nil)
+	if #g>0 then
+		sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_TOGRAVE)
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetLabelObject(e:GetLabelObject())
+	e1:SetOperation(s.thop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if g:HasFlagEffect(id) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g:IsCanBeSpecialSummoned(e,0,tp,tp,false,false,POS_FACEUP) and (g:IsLocation(LOCATION_GRAVE) or g:IsLocation(LOCATION_REMOVED) or (g:IsLocation(LOCATION_EXTRA) and g:IsFaceup())) then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
