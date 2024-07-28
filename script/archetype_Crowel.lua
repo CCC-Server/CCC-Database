e819f9a13866539f750f80289a7aa2b52a47075c
Crowel <faithtaeho@naver.com>
2024-07-28 15:05:19 +0900

    령술사 관련 상수 수정
    0xd05 셋코드는 예약만 해 둡니다.

--- script/archetype_Crowel.lua
+++ script/archetype_Crowel.lua
@@ -3,13 +3,22 @@ Duel.LoadScript("proc_archetype.lua")
 CARD_SPIRITUAL_LIGHT_ART = CARD_SPIRITUAL_LIGHT_ART or 5037726
 CARD_DARK_SPIRIT_ART     = CARD_DARK_SPIRIT_ART     or 38167722
 ARCHETYPE_SPIRITUAL_ART  = SET_SPIRITUAL_ART or ARCHETYPE_SPIRITUAL_ART or 0x14d
-ARCHETYPE_SPIRITUALIST   = SET_SPIRITUALIST  or ARCHETYPE_SPIRITUALIST  or 0x4d01
---ARCHETYPE_SPIRITUAL_LIGHT_ART = 0x9d01 --Unused
---ARCHETYPE_DARK_SPIRIT_ART     = 0xad01 --Unused
-ARCHETYPE_DORIADO        = SET_DORIADO       or ARCHETYPE_DORIADO       or 0xd02
-ARCHETYPES_SPIRITUAL     = 0xd01
-Archetype.MakeCheck(ARCHETYPE_SPIRITUAL_ART,nil,{ARCHETYPES_SPIRITUAL})
-Archetype.MakeCheck(SET_SPIRITUALIST,{
+ARCHETYPES_SPIRITUAL     = {0xd01,0xd05}
+ARCHETYPE_SPIRITUAL_LIGHT_ART = 0x9d01
+ARCHETYPE_DARK_SPIRIT_ART     = 0xcd01
+ARCHETYPE_SPIRITUALIST   = 0xd05 --reserved
+Archetype.MakeCheck(ARCHETYPE_SPIRITUAL_ART,nil,ARCHETYPES_SPIRITUAL)
+Archetype.MakeCheck(ARCHETYPES_SPIRITUAL,{
+	["대령술-「일륜」"]=38057522,
+	["정령술의 마스터"]=91530236
+},nil)
+Archetype.MakeCheck(ARCHETYPE_SPIRITUAL_LIGHT_ART,{
+	["광령술-「성」"]=5037726
+},nil)
+Archetype.MakeCheck(ARCHETYPE_DARK_SPIRIT_ART,{
+	["암령술-\"욕망\""]=5037726
+},nil)
+Archetype.MakeCheck(ARCHETYPE_SPIRITUALIST,{
 	["승령술사 조겐"]=41855169,
 	["묘지기의 영술사"]=58657303,
 	--"령술사" 지정 카드가 없어서 cdb에서 생략한 부분 처리
@@ -21,20 +30,8 @@ Archetype.MakeCheck(SET_SPIRITUALIST,{
 	["풍령술사 윈"]=124061016,
 	["정령술사의 기도"]=124061020
 },nil)
---[[
-Archetype.MakeCheck(ARCHETYPE_SPIRITUAL_LIGHT_ART,{
-	["광령술-「성」"]=5037726,
-	--"광령술" 지정 카드가 없어서 cdb에서 생략한 부분 처리
-	["광령술사 라이너"]=124061011,
-	["광령술-「별」"]=124061027
-},nil)
-Archetype.MakeCheck(ARCHETYPE_DARK_SPIRIT_ART,{
-	["암령술-\"욕망\""]=5037726,
-	--"암령술" 지정 카드가 없어서 cdb에서 생략한 부분 처리
-	["암령술사 달크"]=124061012,
-	["암령술-\"성\""]=124061028
-},nil)
---]]
+--드리야드
+ARCHETYPE_DORIADO = SET_DORIADO or ARCHETYPE_DORIADO or 0xd02
 Archetype.MakeCheck(ARCHETYPE_DORIADO,{
 	["드리야드"]=84916669,
 	["엘리멘틀 마스터 드리야드"]=99414168,
