a33219a15169ac66bead5d78bb39536d83916d71
id187 <limajaka123@naver.com>
2024-08-27 17:25:46 +0900

    수정

--- script/c124161052.lua
+++ script/c124161052.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,nil,4,2)
 	--effefct 1
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
