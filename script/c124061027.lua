1894444986d1db8b4b0fd5c4319a0e692ad1af6d
Crowel <faithtaeho@naver.com>
2024-09-03 19:32:03 +0900

    령술사 리워크 (진행 중)
    - 광령술-「성」: 재작업 완료
    - 나머지 카드도 텍스트 수정은 완료, 스크립트 수정 작업은 차차 진행 예정

--- script/c124061027.lua
+++ script/c124061027.lua
@@ -3,6 +3,7 @@
 Duel.LoadScript("archetype_crowel.lua")
 local s,id=GetID()
 function s.initial_effect(c)
+	--SpSummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,34 +13,43 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-
-function s.filter(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT)
-end
+--SpSummon
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.filter,1,false,nil,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.filter,1,1,false,nil,nil)
+	e:SetLabel(1)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,Card.IsAttribute,1,false,aux.ReleaseCheckMMZ,nil,ATTRIBUTE_LIGHT) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsAttribute,1,1,false,aux.ReleaseCheckMMZ,nil,ATTRIBUTE_LIGHT)
 	Duel.Release(g,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-	return c:IsArchetype(ARCHETYPE_SPIRITUAL_ART) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
+	return c:IsArchetype(ARCHETYPE_SPIRITUAL_ART) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) 
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then
+		if e:GetLabel()==1 then
+			e:SetLabel(0)
+			return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		else
+			return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+				and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		end
+	end
+	e:SetLabel(0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
-	if tc then
-		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)>0 then
-			local seq=tc:GetSequence()
-			if Duel.Remove(tc,nil,REASON_EFFECT|REASON_TEMPORARY)>0 and tc:IsLocation(LOCATION_REMOVED) and not tc:IsReason(REASON_REDIRECT) then
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)>0 then
+		Duel.BreakEffect()
+		if tc:IsImmuneToEffect(e) or not Duel.SelectYesNo(aux.Stringid(id,0))
+			or Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)==0 then
 				Duel.BreakEffect()
-				Duel.ReturnToField(tc,nil,0x1<<seq)
-			end
+				local seq=tc:GetSequence()
+				if Duel.Remove(tc,nil,REASON_EFFECT|REASON_TEMPORARY)>0 and tc:IsLocation(LOCATION_REMOVED) and not tc:IsReason(REASON_REDIRECT) then
+					Duel.ReturnToField(tc,nil,0x1<<seq)
+				end
 		end
 	end
 end
--- script/c124061028.lua
+++ script/c124061028.lua
@@ -1,8 +1,9 @@
 --闇霊術－「星」
 --암령술-"성"
 Duel.LoadScript("archetype_crowel.lua")
-local s,id=GetID()
+localss,id=GetID()
 function s.initial_effect(c)
+	--Set
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -11,25 +12,20 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-
-function s.filter(c)
-	return c:IsAttribute(ATTRIBUTE_DARK)
-end
+--Set
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.filter,1,false,nil,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.filter,1,1,false,nil,nil)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsAttribute,1,false,nil,nil,ATTRIBUTE_DARK) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsAttribute,1,1,false,nil,nil,ATTRIBUTE_DARK)
 	Duel.Release(g,REASON_COST)
 end
 function s.setfilter(c)
-	return c:IsArchetype(ARCHETYPE_SPIRITUAL_ART) and (c:GetType()==TYPE_SPELL+TYPE_QUICKPLAY or c:GetType()==TYPE_TRAP) and c:IsSSetable()
+	return c:IsArchetype(ARCHETYPE_SPIRITUAL_ART) and (c:IsQuickPlaySpell() or c:IsTrap()) and c:IsSSetable()
 end
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and #g>0 end
 end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil)
 	if #g>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
@@ -38,14 +34,13 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		for tc in sg:Iter() do
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
-			if tc:IsQuickPlaySpell() then
-				e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
-			elseif tc:IsTrap() then
-				e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-			end
+			e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
 			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			tc:RegisterEffect(e2)
 		end
 	end
 end
