c24694d0469c6bcf7968e889aba744f279cb289e
ele093 <50264650+ele093@users.noreply.github.com>
2024-09-13 22:30:55 +0900

    서나래님 카드 수정
    미스틱 베이비 3종

--- script/c124131172.lua
+++ script/c124131172.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--이번 턴, 이 카드는 전투로는 파괴되지 않는다.
 	local e2=Effect.CreateEffect(c)
-  	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.rmcon)
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetCountLimit(1,{id,2})
+	e1:SetCountLimit(1,{id,2})
 	e1:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_DECK) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
@@ -97,20 +97,31 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e1:SetRange(LOCATION_MZONE)
-        e1:SetCode(EVENT_PHASE+PHASE_END)
-        e1:SetCondition(s.descon)
-        e1:SetOperation(s.desop2)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        e1:SetCountLimit(1)
-        e:GetHandler():RegisterEffect(e1)
-    end
+		local fid=e:GetHandler():GetFieldID()
+		tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1,fid)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(s.descon)
+		e1:SetOperation(s.desop)
+		e1:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-    return not e:GetHandler():IsHasEffect(66235877)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(id)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
 end
-function s.desop2(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
 end
