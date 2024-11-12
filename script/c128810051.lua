b7ec81705b0a7ff8357981ec64bbbb2dacd00e28
Odin.L.Strange(이서원) <102892589+OdinStrange@users.noreply.github.com>
2024-11-12 21:43:23 +0900

    오류 수정

--- script/c128810051.lua
+++ script/c128810051.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,aux.Synchro.NonTunerEx(s.matfilter),1,99)
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FAIRY),1,1,Synchro.NonTunerEx(s.matfilter),1,99)
 	c:EnableReviveLimit()
 	--pendulum Summon
 	Pendulum.AddProcedure(c,false)
