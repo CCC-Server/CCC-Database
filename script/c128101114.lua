ba87a8c92bd2f4a9657eb7ba6dba64a05bfd21d1
badinamchin <mukai123@naver.com>
2024-11-20 01:11:23 +0900

    주술회전 수정

---
+++ script/c128101114.lua
@@ -0,0 +1,81 @@
+-- gojo satoju
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Can be used as the entire requirement for the Ritual Summon of a LIGHT Warrior or Dragon Ritual Monster
+	Ritual.AddWholeLevelTribute(c,aux.FilterBoolFunction(Card.IsSetCard,0xc41))
+
+	-- Special Summon itself from the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return not e:GetHandler():IsReason(REASON_DRAW) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+
+	-- Apply the effects of a "Ritual" spell
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(_,tp) return Duel.IsTurnPlayer(1-tp) end)
+	e2:SetCost(s.ritcost)
+	e2:SetTarget(s.rittg)
+	e2:SetOperation(s.ritop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xc41}
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.ritcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
+end
+
+function s.ritfilter(c)
+	return c:IsAbleToGraveAsCost() and c:IsSetCard(0xc41) and c:IsType(TYPE_SPELL) and c:IsType(TYPE_RITUAL) and c:CheckActivateEffect(true,true,false)~=nil
+end
+
+function s.rittg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		if e:GetLabel()==0 then return false end
+		e:SetLabel(0)
+		return Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_DECK,0,1,nil)
+	end
+	e:SetLabel(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tc=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	local te=tc:CheckActivateEffect(true,true,false)
+	e:SetLabelObject(te)
+	Duel.SendtoGrave(tc,REASON_COST)
+	e:SetProperty(te:GetProperty())
+	local tg=te:GetTarget()
+	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	Duel.ClearOperationInfo(0)
+end
+
+function s.ritop(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	local op=te:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+end
