1a26c849de26778d52ed41674ade3decc396d443
badinamchin <mukai123@naver.com>
2024-11-26 22:53:02 +0900

    봇치 카드 수정

---
+++ script/c128101007.lua
@@ -0,0 +1,72 @@
+--히토리 & 이쿠요
+local s,id=GetID()
+function s.initial_effect(c)
+    --Fusion summon procedure
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,128101000,128101005)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.negcon)
+    e1:SetTarget(s.negtg)
+    e1:SetOperation(s.negop)
+    c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_LEAVE_FIELD)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2
+end
+s.listed_names={128101000,128101005}
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and (re:GetActivateLocation()==LOCATION_ONFIELD or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and rp==1-tp
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.NegateEffect(ev)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return c:IsPreviousPosition(POS_FACEUP) and c:IsSummonType(SUMMON_TYPE_SPECIAL) and c:IsPreviousLocation(LOCATION_MZONE)
+        and c:IsPreviousControler(tp) and rp==1-tp
+end
+function s.spfilter(c,e,tp)
+    return (c:IsCode(128101000) or c:IsSetCard(0xc40)) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
+        Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+    Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thfilter(c)
+    return c:ListsCode(128101000) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        if g:GetFirst():IsCode(128101000) then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+            local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+            if #hg>0 then
+                Duel.SendtoHand(hg,nil,REASON_EFFECT)
+                Duel.ConfirmCards(1-tp,hg)
+            end
+        end
+    end
+end
