db11d086b6a5c65950c44ae0ec2739eb4f7c4028
Crowel <faithtaeho@naver.com>
2024-11-28 12:06:44 +0900

    스크립트 테스트용 스킬 추가 (제작 중)
    - 의뢰받은 기능 (②효과) 은 구현 완료
    - 추가 기능 (①효과) 은 차후 구현 예정
    - 이미지 및 스킬 카드용 뒷면은 차후 제작 예정

---
+++ script/c214720000.lua
@@ -0,0 +1,98 @@
+--Custom Card Creators
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local c=e:GetHandler()
+	if Duel.GetFlagEffect(tp,id)==0 then
+		--Create catd & To field
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCondition(s.con)
+		e1:SetOperation(s.op)
+		Duel.RegisterEffect(e1,tp)
+	end
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+end
+function s.mftfilter(c,mft1,mft2,mft3)
+	if not c:IsLocation(LOCATION_EXTRA) then
+		return mft1>0
+	elseif c:IsLinkMonster() or c:IsFaceup() then
+		return mft2>0
+	end
+	return mft3>0
+end
+function s.filter(c,e,tp,mft,sft)
+	if c:IsLocation(LOCATION_REMOVED) and c:IsFacedown() then return false end
+	local b1=c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,true,false) and s.mftfilter(c,table.unpack(mft))
+	local b2=(c:IsFieldSpell() or (sft>0 and c:IsContinuousSpellTrap()))
+		and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+	local b3=(c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) and s.mftfilter(c,table.unpack(mft)))
+		or ((c:IsFieldSpell() or (sft>0 and c:IsSpellTrap())) and c:IsSSetable())
+	return (b1 or b2 or b3)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	local mft={Duel.GetLocationCount(tp,LOCATION_MZONE),Duel.GetLocationCountFromEx(tp),Duel.GetUsableMZoneCount(tp)}
+	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	--Create card
+	local opt1=true
+	--To field
+	local opt2=Duel.IsExistingMatchingCard(s.filter,tp,0x73,0,1,nil,e,tp,mft,sft)
+	return (opt1 or opt2)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local mft={Duel.GetLocationCount(tp,LOCATION_MZONE),Duel.GetLocationCountFromEx(tp),Duel.GetUsableMZoneCount(tp)}
+	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	--Create card
+	local opt1=true
+	--To field
+	local opt2=Duel.IsExistingMatchingCard(s.filter,tp,0x73,0,1,nil,e,tp,mft,sft)
+	if not (opt1 or opt2) then return end
+	local op=Duel.SelectEffect(tp,
+		{opt1,aux.Stringid(id,0)},
+		{opt2,aux.Stringid(id,1)})
+	if op==1 then
+		--나중에 구현!
+		Duel.TossCoin(tp,1)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+		local tc=Duel.SelectMatchingCard(tp,s.filter,tp,0x73,0,1,1,nil,e,tp,mft,sft):GetFirst()
+		local b1=tc:IsMonster() and tc:IsCanBeSpecialSummoned(e,0,tp,true,false) and s.mftfilter(tc,table.unpack(mft))
+		local b2=tc:IsFieldSpell() and not tc:IsForbidden() and tc:CheckUniqueOnField(tp)
+		local b3=sft>0 and tc:IsContinuousSpellTrap() and not tc:IsForbidden() and tc:CheckUniqueOnField(tp)
+		local b4=(tc:IsMonster() and tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) and s.mftfilter(tc,table.unpack(mft)))
+			or ((tc:IsFieldSpell() or (sft>0 and tc:IsSpellTrap())) and tc:IsSSetable())
+		local op2=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,2)},
+			{b2,aux.Stringid(id,3)},
+			{b3,aux.Stringid(id,4)},
+			{b4,aux.Stringid(id,5)})
+		if op2==1 then
+			Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
+		elseif op2==2 then
+			Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+		elseif op2==3 then
+			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		else
+			if tc:IsMonster() then
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+				Duel.ConfirmCards(1-tp,tc)
+			else
+				Duel.SSet(tp,tc)
+			end
+		end
+	end
+end
