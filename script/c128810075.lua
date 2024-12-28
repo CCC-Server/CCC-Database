451d4f6174645d82677bb4c898aa570fbc4f092a
OdinStrange <102892589+OdinStrange@users.noreply.github.com>
2024-12-28 21:51:09 +0900

    스크립트 고침

--- script/c128810075.lua
+++ script/c128810075.lua
@@ -5,22 +5,28 @@ function s.initial_effect(c)
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1,1,Synchro.NonTunerEx(Card.IsSetCard,0xc03),1,1)
 	c:EnableReviveLimit()
 --activate cost
+local e=Effect.CreateEffect(c)
+	--activate cost
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_ACTIVATE_COST)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(0,1)
-	e1:SetCondition(s.costtarget)
 	e1:SetCost(s.costchk)
 	e1:SetOperation(s.costop)
 	c:RegisterEffect(e1)
-
+	--accumulate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(id)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(0,1)
+	c:RegisterEffect(e2) 
 end
 s.listed_series={0xc03}
-function s.costtarget(e,c)
-	local c=e:GetHandler()
-end
+
 function s.costchk(e,te_or_c,tp)
 	local ct=#{Duel.GetPlayerEffect(tp,id)}
 	return Duel.CheckLPCost(tp,ct*500)
