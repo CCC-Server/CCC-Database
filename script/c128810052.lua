37aa22009f74c55bd1f48ce5e7ce6a92d24dbba5
OdinStrange <102892589+OdinStrange@users.noreply.github.com>
2025-02-07 20:23:41 +0900

    이터니티 소조, 효과 수정

--- script/c128810052.lua
+++ script/c128810052.lua
@@ -2,11 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	
-	local synchro_proc0=Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FAIRY),2,99,Synchro.NonTunerEx(s.matfilter2),1,1)
-	local synchro_proc1=Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(s.matfilter),1,1,Synchro.NonTunerEx(s.matfilter2),1,1)
-	synchro_proc0:SetDescription(aux.Stringid(id,0))
-	synchro_proc1:SetDescription(aux.Stringid(id,1))
+	Synchro.AddProcedure(c,nil,2,2,Synchro.NonTunerEx(s.matfilter),1,1)
 	c:AddMustBeSynchroSummoned()
 	Pendulum.AddProcedure(c,false)
 	--Cannot be destroyed by effects
@@ -18,68 +14,44 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--Negate the activation of another card or effect and destroy it
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,3))
-	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) end)
-	e2:SetTarget(s.negtg)
-	e2:SetOperation(s.negop)
-	c:RegisterEffect(e2)
-	--Multiple Tuner check
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_MATERIAL_CHECK)
-	e3:SetValue(s.valcheck)
-	c:RegisterEffect(e3)
+	--Change a monster effect activated by the opponent
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp and re:IsMonsterEffect() end)
+	e4:SetCost(s.cost)
+	e4:SetTarget(s.chngtg)
+	e4:SetOperation(s.chngop)
+	c:RegisterEffect(e4)
 end
 s.listed_series={0xc02}
 s.synchro_nt_required=1
-s.synchro_tuner_required=1
 function s.matfilter(c,val,scard,sumtype,tp)
 	return c:IsRace(RACE_FAIRY,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
 end
-function s.matfilter2(c,val,scard,sumtype,tp)
-	return c:IsRace(RACE_FAIRY,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,500) end
+	Duel.PayLPCost(tp,1000)
 end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local rc=re:GetHandler()
-	if chk==0 then return rc~=e:GetHandler() end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,tp,0)
-	if rc:IsRelateToEffect(re) and rc:IsDestructable() then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,tp,0)
-	end
+function s.fairyfilter(c)
+	return c:IsRace(RACE_FAIRY) and c:IsMonster() and (c:IsFaceup() or not c:IsOnField())
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
-		local atk=rc:IsMonster() and rc:GetTextAttack() or 0
-		if atk<0 then atk=0 end
-		local c=e:GetHandler()
-		if Duel.Destroy(rc,REASON_EFFECT)>0 and atk>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
-			--This card gains ATK equal to that monster's original ATK until the end of this turn
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
-			c:RegisterEffect(e1)
-		end
-	end
+function s.chngtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fairyfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
 end
-function s.valcheck(e,c)
-	local g=c:GetMaterial()
-	if g:IsExists(Card.IsType,2,nil,TYPE_TUNER) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-		e1:SetCode(EFFECT_MULTIPLE_TUNERS)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~(RESET_TOFIELD)|RESET_PHASE|PHASE_END)
-		c:RegisterEffect(e1)
+function s.chngop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.repop)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(1-tp,s.fairyfilter,1-tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SpecialSummon(g,REASON_EFFECT,LOCATION_GRAVE,1-tp)
 	end
 end
