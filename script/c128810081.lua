9d17a1bcd7fb267ecc4909d8b76cbf19f135811f
OdinStrange <102892589+OdinStrange@users.noreply.github.com>
2025-04-24 22:33:09 +0900

    효과 조정

--- script/c128810081.lua
+++ script/c128810081.lua
@@ -14,25 +14,23 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0xc02}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xc02) and c:IsType(TYPE_SYNCHRO)
+end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsMonsterEffect()) and Duel.IsChainNegatable(ev)
-		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FAIRY),tp,LOCATION_MZONE,0,1,nil)
+	return (re:IsMonsterEffect() or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,eg,1,tp,0)
+	local rc=re:GetHandler()
+	if rc:IsDestructable() and rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function s.opfilter(c)
-	return c:IsFaceup() and c:IsMonster() and c:IsSetCard(0xc02) and c:IsType(TYPE_SYNCHRO)
-end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
-		and Duel.IsExistingMatchingCard(s.opfilter,tp,LOCATION_ONFIELD|LOCATION_GRAVE,0,1,nil)
-		and re:GetHandler():IsDestructable() and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		Duel.BreakEffect()
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
