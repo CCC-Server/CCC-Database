b3c31306a76e321640d140895685be147e185428
badinamchin <mukai123@naver.com>
2025-04-30 22:20:34 +0900

    Update c128101142.lua

--- script/c128101142.lua
+++ script/c128101142.lua
@@ -1,10 +1,10 @@
 --Blaze Cannon Bringer
 local s,id=GetID()
 function s.initial_effect(c)
-	--① Set 1 "Blaze Accelerator" Continuous Spell/Trap from Deck
+	--① 패 / 필드에서 자신을 묘지로 → 브레이즈 캐논 지속 마법/함정 덱에서 앞면 세트
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOFIELD)
+	-- e1:SetCategory(CATEGORY_TOFIELD) ← 오류 발생하므로 삭제
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
 
-	--② Special Summon self + Summon Bomb Token to opponent's field
+	--② 묘지에서 발동: 상대 필드에 토큰 소환 후 자신 부활
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
@@ -28,18 +28,21 @@ end
 s.listed_names={69537999} -- 브레이즈 캐논
 s.listed_series={SET_VOLCANIC}
 
---① 코스트: 자신 묘지로
+--① 비용: 자신 묘지로
 function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
+
 function s.setfilter(c)
 	return c:IsCode(69537999) and c:IsType(TYPE_CONTINUOUS) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsForbidden()
 end
+
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
 end
+
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
@@ -50,10 +53,11 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---② 조건: 필드에 브레이즈 캐논이 존재
+--② 조건: 자신 필드에 브레이즈 캐논 존재
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(function(c) return c:IsFaceup() and c:IsCode(69537999) end,tp,LOCATION_ONFIELD,0,1,nil)
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
@@ -65,15 +69,16 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,1-tp,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	-- 폭탄 토큰 생성
+	-- 상대 필드에 토큰 소환
 	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x32,TYPE_TOKEN,1000,1000,1,RACE_PYRO,ATTRIBUTE_FIRE,POS_FACEUP,1-tp) then
 		local token=Duel.CreateToken(tp,id+1)
 		Duel.SpecialSummon(token,0,tp,1-tp,false,false,POS_FACEUP)
 	end
-	-- 자신 특소
+	-- 자신 특수 소환
 	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
