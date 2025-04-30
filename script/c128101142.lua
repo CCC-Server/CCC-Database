0e2f44072ae47f8ec9b2414eb9c1e8036ad2c704
badinamchin <mukai123@naver.com>
2025-04-30 22:41:56 +0900

    Update c128101142.lua

--- script/c128101142.lua
+++ script/c128101142.lua
@@ -1,87 +1,86 @@
---Blaze Cannon Bringer
+-- 카드명: (예시) "브레이즈 캐논 서포트 몬스터"
+-- Blaze Cannon Support Monster
 local s,id=GetID()
 function s.initial_effect(c)
-	--① 패/필드에서 자신 묘지로 → 브레이즈 캐논 지속 마/함 덱에서 앞면 세트
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(s.setcost)
-	e1:SetTarget(s.settg)
-	e1:SetOperation(s.setop)
-	c:RegisterEffect(e1)
-
-	--② 묘지에서 발동: 상대 필드에 토큰 소환 후 자신 부활
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id+100)
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
-end
-s.listed_names={69537999,id}
-s.listed_series={SET_VOLCANIC}
-
---① 코스트: 자신을 묘지로
-function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+    -- ① 효과: 패/필드 → 묘지, 덱에서 "Volcanic Blaze Accelerator" 세트
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOGRAVE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetCost(s.tgcost)
+    e1:SetTarget(s.settg)
+    e1:SetOperation(s.setop)
+    c:RegisterEffect(e1)
+    
+    -- ② 효과: 묘지에서 특소 + 상대 필드에 토큰 생성
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
 end
 
---① 타겟 설정: 덱에 브레이즈 캐논 지속 마/함 존재 여부 확인
-function s.setfilter(c)
-	return c:IsCode(69537999) and c:IsType(TYPE_CONTINUOUS) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsForbidden()
+-- ① 효과 코스트: 묘지로 보내기
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+
+-- ① 효과 타겟: 덱에서 "Volcanic Blaze Accelerator" 검색
+function s.setfilter(c,tp)
+    return c:IsCode(69537999) and (c:IsContinuousSpell() or c:IsContinuousTrap()) 
+        and not c:IsForbidden() and c:CheckUniqueOnField(tp)
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-			and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil)
-	end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil,tp) end
 end
-
---① 처리: 덱에서 브레이즈 캐논 지속 마/함 1장 앞면으로 세트
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-	end
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+    local tc=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+    if tc then
+        Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+    end
 end
 
---② 조건: 자신 필드에 브레이즈 캐논이 앞면 존재할 때
+-- ② 효과 조건: 자신 필드에 "Volcanic Blaze Accelerator" 존재
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(function(c) return c:IsFaceup() and c:IsCode(69537999) end,tp,LOCATION_ONFIELD,0,1,nil)
+    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_SZONE,0,1,nil)
+end
+function s.cfilter(c)
+    return c:IsFaceup() and c:IsCode(69537999) -- "Volcanic Blaze Accelerator"의 ID
 end
 
---② 대상 확인
+-- ② 효과 실행: 토큰 생성 + 자신 특소
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-			and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x32,TYPE_TOKEN,1000,1000,1,RACE_PYRO,ATTRIBUTE_FIRE,POS_FACEUP,1-tp)
-			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,1-tp,LOCATION_MZONE)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+    local c=e:GetHandler()
+    if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,1000,1000,1,RACE_PYRO,ATTRIBUTE_FIRE,POS_FACEUP,1-tp)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_GRAVE)
 end
-
---② 처리: 폭탄 토큰을 상대 필드에 특수 소환하고, 자신도 특수 소환
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x32,TYPE_TOKEN,1000,1000,1,RACE_PYRO,ATTRIBUTE_FIRE,POS_FACEUP,1-tp) then
-		local token=Duel.CreateToken(tp,id+1)
-		Duel.SpecialSummon(token,0,tp,1-tp,false,false,POS_FACEUP)
-	end
-	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	end
+    -- 상대 필드에 토큰 생성 (폭탄 토큰: 화염족/레벨1/공1000/수1000)
+    if Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0 then return end
+    local token=Duel.CreateToken(tp,id+1) -- 토큰 ID는 이 카드 ID +1로 가정
+    if Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP) then
+        -- 토큰 속성 설정 (필요시 추가)
+    end
+    Duel.SpecialSummonComplete()
+    
+    -- 자신의 묘지에서 이 카드 특소
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
