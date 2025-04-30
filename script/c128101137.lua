0799d59404839aaf2354abb5009fae5cf087ebef
badinamchin <mukai123@naver.com>
2025-04-30 20:20:43 +0900

    Update c128101137.lua

--- script/c128101137.lua
+++ script/c128101137.lua
@@ -1,119 +1,122 @@
---ReGLOSS Overfusion Entity
+--카드명: 트라미드 넥서리온 스핑크스
 local s,id=GetID()
 function s.initial_effect(c)
-	-- 융합 소환 제한
-	c:EnableReviveLimit()
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetValue(aux.fuslimit)
-	c:RegisterEffect(e0)
+    c:EnableReviveLimit()
+    --융합 소환 조건
+    Fusion.AddProcMix(c,true,true,s.ffilter1,s.ffilter2)
 
-	-- 룰 특수 소환 (세트된 마/함 2장 묘지로)
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(s.sprcon)
-	e1:SetOperation(s.sprop)
-	c:RegisterEffect(e1)
+    -- 특수 소환 제한
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e0:SetValue(s.splimit)
+    c:RegisterEffect(e0)
 
-	-- ① 특소 성공 시 → ReGLOSS 몬스터 세트
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.setcon)
-	e2:SetTarget(s.settg)
-	e2:SetOperation(s.setop)
-	c:RegisterEffect(e2)
+    -- 필드의 몬스터 + 필드 마법으로 특수 소환
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_EXTRA)
+    e1:SetCondition(s.spcon)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
 
-	-- ② 상대 턴 묘지에서 특소
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_GRAVE)
-	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e3:SetCountLimit(1,id+100)
-	e3:SetCondition(s.spcon)
-	e3:SetCost(s.spcost)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-	c:RegisterEffect(e3)
+    -- (1) 특수 소환 시 필드 마법 발동
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCountLimit(1,id)
+    e2:SetOperation(s.activatefield)
+    c:RegisterEffect(e2)
+
+    -- (2) 자신의 카드가 묘지로 갈 경우 상대 카드 제외
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCondition(s.rmcon)
+    e3:SetTarget(s.rmtg)
+    e3:SetOperation(s.rmop)
+    c:RegisterEffect(e3)
 end
-
--- 변경된 부분: 세트된 마/함 인식에 GetType() 사용
-function s.spcfilter(c)
-	return c:IsFacedown()
-		and bit.band(c:GetType(), TYPE_SPELL+TYPE_TRAP) ~= 0
-		and c:IsAbleToGraveAsCost()
+s.listed_names={id}
+s.listed_series={SET_TRIAMID}
+-- 융합 소재
+function s.ffilter1(c,fc,sumtype,tp)
+    return c:IsSetCard(SET_TRIAMID)  -- 트라미온 또는 트라미드
 end
-
--- 특수 소환 조건
-function s.sprcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_SZONE,0,2,nil)
-end
-function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_SZONE,0,2,2,nil)
-	Duel.SendtoGrave(g,REASON_COST)
+function s.ffilter2(c,fc,sumtype,tp)
+    return c:IsRace(RACE_ROCK)
 end
 
--- ① 엑덱 특소시 → 덱에서 ReGLOSS 몬스터 세트
-function s.setcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonLocation(LOCATION_EXTRA)
-end
-function s.setfilter(c)
-	return c:IsSetCard(0x306a) and c:IsMonster() and c:IsSSetable()
-end
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
-end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SSet(tp,g:GetFirst())
-	end
+-- 특수 소환 제한: 융합 또는 지정 방식만 가능
+function s.splimit(e,se,sp,st)
+    return se:IsHasType(EFFECT_TYPE_ACTION) or e:GetHandler():IsLocation(LOCATION_EXTRA)
 end
 
--- ② 상대 턴 조건
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
+-- 특수 소환 조건: 트라미드 몬스터 + 트라미온 + 필드 마법 묘지로
+function s.spfilter1(c)
+    return c:IsSetCard(SET_TRIAMID) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_SZONE,0,1,nil) end
-	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_SZONE,0,1,1,nil)
-	Duel.SendtoGrave(g,REASON_COST)
+function s.spfilter2(c)
+    return c:IsSetCard(SET_TRIAMID) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+function s.spfilter3(c)
+    return c:IsType(TYPE_FIELD) and c:IsAbleToGraveAsCost()
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		-- 이탈 시 제외
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
-		e1:SetValue(LOCATION_REMOVED)
-		c:RegisterEffect(e1,true)
-	end
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    return Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_ONFIELD,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_ONFIELD,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.spfilter3,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_ONFIELD,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_ONFIELD,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g3=Duel.SelectMatchingCard(tp,s.spfilter3,tp,LOCATION_ONFIELD,0,1,1,nil)
+    g1:Merge(g2)
+    g1:Merge(g3)
+    Duel.SendtoGrave(g1,REASON_COST)
+end
+
+-- (1) 특수 소환 성공 시: 트라미온 필드 마법 발동
+function s.fieldfilter(c,tp)
+    return c:IsType(TYPE_FIELD) and c:IsSetCard(SET_TRIAMID) and not c:IsForbidden()
+        and c:CheckActivateEffect(false,true,false)~=nil
+end
+function s.activatefield(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+    local g=Duel.SelectMatchingCard(tp,s.fieldfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+    end
+end
+
+-- (2) 자신의 카드가 묘지로 → 상대 카드 제외
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(Card.IsControler,1,nil,tp)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+    end
 end
