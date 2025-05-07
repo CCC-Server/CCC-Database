5e1c4f6d7308418fc9c0381949224f40445377b2
badinamchin <mukai123@naver.com>
2025-05-07 15:57:16 +0900

    Update c128101146.lua

--- script/c128101146.lua
+++ script/c128101146.lua
@@ -1,83 +1,86 @@
---볼캐닉 포워더
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableReviveLimit()
-	--링크 소환
-	Link.AddProcedure(c,s.matfilter,1,1,true)
+    --Link Summon
+    Link.AddProcedure(c,s.matfilter,1,1,true)
+    c:EnableReviveLimit()
 
-	--서치
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.thcon)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
+    --Add 1 "Volcanic" monster from Deck to hand
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCondition(s.thcon)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
 
-	--묘지로 갔을 때 데미지 + 세트
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DAMAGE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(s.damtg)
-	e2:SetOperation(s.damop)
-	c:RegisterEffect(e2)
+    --Burn and set Blaze Accelerator
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_DAMAGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e2:SetTarget(s.damtg)
+    e2:SetOperation(s.damop)
+    c:RegisterEffect(e2)
 end
 
---링크 소재 필터
-function s.matfilter(c,lc,sumtype,tp)
-	return c:IsSetCard(0x32) and c:IsLevelBelow(4)
+-- ✅ 수정된 matfilter
+function s.matfilter(c)
+    return c:IsSetCard(0x32) and c:IsLevelBelow(4)
 end
 
---① 서치 조건: 링크 소환으로 나왔을 경우
+-- Add to hand
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x32) and c:IsAbleToHand()
+    return c:IsSetCard(0x32) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
 end
 
---② 묘지로 갔을 때
-function s.damfilter(c,tp)
-	return c:IsFaceup() and c:IsRace(RACE_PYRO) and c:IsControler(tp)
-end
-function s.setfilter(c)
-	return c:IsSetCard(SET_BLAZE_ACCELERATOR) and (c:IsType(TYPE_CONTINUOUS) or c:IsType(TYPE_TRAP)) and not c:IsForbidden()
-end
+-- Burn + Set Blaze Accelerator
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.damfilter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(function(c) return s.damfilter(c,tp) end,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,function(c) return s.damfilter(c,tp) end,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.damfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.damfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,s.damfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetLevel()*100)
+end
+
+function s.damfilter(c)
+    return c:IsFaceup() and c:IsRace(RACE_PYRO)
+end
+
+function s.setfilter(c)
+    return c:IsSetCard(SET_BLAZE_ACCELERATOR) and c:IsSpellTrap() and c:IsSSetable()
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local atk=tc:GetLevel()*100
-		Duel.Damage(1-tp,atk,REASON_EFFECT)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-		if #g>0 then
-			Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		end
-	end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        local atk=tc:GetLevel()*100
+        Duel.Damage(1-tp,atk,REASON_EFFECT)
+        local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
+        if #g>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+            local sg=g:Select(tp,1,1,nil)
+            if #sg>0 then
+                Duel.SSet(tp,sg:GetFirst())
+            end
+        end
+    end
 end
