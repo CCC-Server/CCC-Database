5eaf8e6f2b6284edc5c329bf1b3e1b5d2dc86fec
badinamchin <mukai123@naver.com>
2025-05-07 14:56:39 +0900

    Add files via upload

---
+++ script/c128101143.lua
@@ -0,0 +1,128 @@
+--볼캐닉 데블건
+--Volcanic Devilgun
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- 특수 소환 조건
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(s.splimit)
+	c:RegisterEffect(e0)
+
+	-- 특수 소환 룰
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+	--①: 특수 소환 성공시 전개+데미지
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+
+	--②: 전투 실행시 효과 봉인
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.actcon)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+
+	--③: 빠른 효과로 브레이즈 캐논 덤핑하고 공격력 상승
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOGRAVE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.atktg)
+	e4:SetOperation(s.atkop)
+	c:RegisterEffect(e4)
+end
+
+s.listed_series={SET_VOLCANIC,SET_BLAZE_ACCELERATOR}
+
+-- 특수 소환 조건: 일반 소환 불가
+function s.splimit(e,se,sp,st)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_GRAVE)
+end
+
+-- 특수 소환 조건: 레벨 8 이상의 화염족 릴리스
+function s.spfilter(c)
+	return c:IsRace(RACE_PYRO) and c:IsLevelAbove(8) and c:IsReleasable()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+
+--①: 특수 소환 성공시 → 상대 필드 전부 파괴 + 1000 데미지
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+		Duel.Damage(1-tp,1000,REASON_EFFECT)
+	end
+end
+
+--②: 이 카드가 전투를 실행할 경우, 상대는 데미지 스텝 종료시까지 카드 효과 발동 불가
+function s.actcon(e)
+	local ph=Duel.GetCurrentPhase()
+	local c=e:GetHandler()
+	return Duel.GetAttacker()==c or Duel.GetAttackTarget()==c and ph>=PHASE_DAMAGE and ph<=PHASE_DAMAGE_CAL
+end
+
+--③: 브레이즈 캐논 덤핑하고 볼캐닉 몬스터 공격력 상승
+function s.tgfilter(c)
+	return c:IsSetCard(SET_BLAZE_ACCELERATOR) and c:IsAbleToGrave()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+		local g2=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_VOLCANIC),tp,LOCATION_MZONE,0,nil)
+		local c=e:GetHandler()
+		local tc=g2:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			tc=g2:GetNext()
+		end
+	end
+end
