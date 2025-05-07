8d1116741d46624c1576339d9641d69082d0d81b
badinamchin <mukai123@naver.com>
2025-05-07 15:38:39 +0900

    Update c128101144.lua

--- script/c128101144.lua
+++ script/c128101144.lua
@@ -1,11 +1,10 @@
---볼캐닉 융합 몬스터
+--볼캐닉 인페르노 타이탄
 local s,id=GetID()
+local SET_BLAZE_ACCELERATOR=0xb9 -- Blaze Accelerator
+
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--융합 소환
-	Fusion.AddProcMixN(c,true,true,s.matfilter,3)
-
-	--블레이즈 캐논 존재시 대체 특수 소환
+	-- Special Summon proc from Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -15,47 +14,43 @@ function s.initial_effect(c)
 	e1:SetOperation(s.sprop)
 	c:RegisterEffect(e1)
 
-	--①: 특수 소환 성공시 덱에서 볼캐닉 몬스터 묘지로
+	-- Send 1 "Volcanic" monster from Deck to GY
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.tgcon)
 	e2:SetTarget(s.tgtg)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 
-	--②: 서로의 턴에 필드 몬스터 파괴 + 화염족 특수 소환
+	-- Destroy and Special Summon
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,id+100)
-	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={SET_VOLCANIC,SET_BLAZE_ACCELERATOR}
--- 융합 소재 필터
-function s.matfilter(c,fc,sumtype,tp)
-	return c:IsSetCard(SET_VOLCANIC)
-end
 
--- 대체 특수 소환 조건: 블레이즈 캐논 + 볼캐닉 2장 묘지로
+-- Special Summon procedure
 function s.sprfilter(c,tp)
-	return c:IsSetCard(SET_VOLCANIC) and c:IsAbleToGraveAsCost() and c:IsMonster()
+	return c:IsSetCard(0x32) and c:IsMonster() and c:IsAbleToGraveAsCost()
 end
 function s.sprcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(c:IsSetCard(SET_BLAZE_ACCELERATOR)),tp,LOCATION_ONFIELD,0,1,nil) -- 블레이즈 캐논
+	return Duel.IsExistingMatchingCard(function(c)
+		return c:IsFaceup() and c:IsSetCard(SET_BLAZE_ACCELERATOR)
+	end,tp,LOCATION_ONFIELD,0,1,nil)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
 		and Duel.IsExistingMatchingCard(s.sprfilter,tp,LOCATION_MZONE,0,2,nil,tp)
 end
@@ -65,10 +60,9 @@ function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 
---①: 특수 소환 성공시
+-- Effect ①: send 1 "Volcanic" monster from Deck to GY
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+SUMMON_TYPE_FUSION)
-		or e:GetHandler():IsSummonLocation(LOCATION_EXTRA)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1)
 end
 function s.tgfilter(c)
 	return c:IsSetCard(0x32) and c:IsMonster() and c:IsAbleToGrave()
@@ -85,31 +79,27 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---②: 필드 몬스터 파괴 + 화염족 특수 소환
+-- Effect ②: destroy and special summon
 function s.desfilter(c)
-	return c:IsFaceup() and c:IsMonster()
+	return c:IsFaceup()
 end
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_PYRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and s.desfilter(chkc) end
-	if chk==0 then
-		return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.desfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if #g>0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
