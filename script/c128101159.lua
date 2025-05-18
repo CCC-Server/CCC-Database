32caa6f1a70d8a6b497a067e361319b1c0bb17b0
badinamchin <mukai123@naver.com>
2025-05-18 22:59:33 +0900

    Update c128101159.lua

--- script/c128101159.lua
+++ script/c128101159.lua
@@ -1,19 +1,17 @@
 --RR-블레이드 레이니어스
 local s,id=GetID()
 function s.initial_effect(c)
-	-- ① 패에서 특수 소환
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1)
+	-- ① 룰 특수 소환: 조건 만족 시 패에서 특수 소환 가능 (1턴 1회 제한)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH) -- ✅ 1턴 1회 제한
+	e0:SetCondition(s.spcon)
+	c:RegisterEffect(e0)
 
-	-- ② 소환 성공 시 RR 마함 서치
+	-- ② 소환 성공 시 RR 마법/함정 서치
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -27,35 +25,23 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 
-	-- ③ 엑시즈 소재로 묘지로 갔을 때 다음 스탠바이 페이즈에 패로
+	-- ③ 엑시즈 소재로 묘지로 간 경우 → 다음 턴 스탠바이 페이즈에 패로 회수
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetCountLimit(1,id+200)
-	e4:SetCondition(s.retcon)
-	e4:SetOperation(s.retop)
+	e4:SetCondition(s.regcon)
+	e4:SetOperation(s.regop)
 	c:RegisterEffect(e4)
 end
 
--- ① 패 특수 소환 조건: 자신 필드에 어둠/비행야수족 이외가 없어야 함
+-- ① 룰 특수 소환 조건: 자신의 필드에 어둠/비행야수 이외 몬스터가 없을 경우
 function s.spfilter(c)
 	return not (c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WINGEDBEAST))
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and not Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
 
 -- ② RR 마법/함정 서치
@@ -75,37 +61,42 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- ③ 엑시즈 소재로 묘지로 갔는지 확인
-function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+-- ③ 엑시즈 소재로 묘지로 간 경우 → 다음 턴(자신/상대)의 스탠바이 페이즈에 발동 가능
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_OVERLAY)
 end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetTurnPlayer()==tp then
-		-- 다음 자신의 스탠바이 페이즈
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,0,2)
-	else
-		-- 상대 턴에 묘지로 간 경우, 1턴 뒤의 자신의 스탠바이 페이즈
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,0,1)
-	end
+	local turn_count = Duel.GetTurnCount()
+	-- 현재 턴 번호 저장
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,0,2,turn_count)
+
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e1:SetCountLimit(1)
-	e1:SetLabelObject(c)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,id+200)
 	e1:SetCondition(s.retcon2)
-	e1:SetOperation(s.retop2)
+	e1:SetTarget(s.rettg)
+	e1:SetOperation(s.retop)
 	e1:SetReset(RESET_PHASE+PHASE_STANDBY,2)
-	Duel.RegisterEffect(e1,tp)
+	c:RegisterEffect(e1)
 end
 function s.retcon2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetLabelObject()
-	return Duel.GetTurnPlayer()==tp and c:GetFlagEffect(id)>0
+	local c=e:GetHandler()
+	local stored_turn = c:GetFlagEffectLabel(id)
+	return Duel.GetTurnCount() > stored_turn
 end
-function s.retop2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetLabelObject()
-	if c:IsRelateToEffect(e) and c:IsLocation(LOCATION_GRAVE) then
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
 		Duel.SendtoHand(c,nil,REASON_EFFECT)
 	end
 end
