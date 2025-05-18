179963c8a0294df332a5b410d683c01a3caf031b
badinamchin <mukai123@naver.com>
2025-05-18 14:12:16 +0900

    Update c128101150.lua

--- script/c128101150.lua
+++ script/c128101150.lua
@@ -1,13 +1,14 @@
--- A・O・J 시뮬레이터 (예시 이름)
+-- A・O・J 시뮬레이터
 local s,id=GetID()
 function s.initial_effect(c)
-	-- ① 패에서 묘지로 보내고 기계족 싱크로 + 파괴
+	-- ① 패에서 묘지로 보내고 싱크로 + 파괴
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.sscost)
 	e1:SetTarget(s.sstg)
@@ -25,7 +26,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 
-	-- 이 카드가 묘지로 보내진 턴 저장
+	-- ③ 묘지로 간 턴 추적용
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_TO_GRAVE)
@@ -33,54 +34,56 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 
--- ■ ① 비용: 패에서 묘지로 자신 보내기
+-- ■ ① 비용: 패에서 묘지
 function s.sscost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 
--- 싱크로 소환 타겟 설정 (기계족)
+-- ■ ① 싱크로 타겟 조건
 function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 		return Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
-			and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_ONFIELD)
 end
 
--- 싱크로 조건 기계족
+-- 기계족 싱크로 조건
 function s.synfilter(c,mg)
 	return c:IsRace(RACE_MACHINE) and c:IsType(TYPE_SYNCHRO) and c:IsSynchroSummonable(nil,mg)
 end
 
+-- ■ ① 싱크로 + 파괴
 function s.ssop(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local sg=Duel.GetMatchingGroup(s.synfilter,tp,LOCATION_EXTRA,0,nil,mg)
+	if #sg==0 then return end
+
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg)
-	local sc=sg:GetFirst()
-	if sc then
-		Duel.SynchroSummon(tp,sc,nil,mg)
-		-- 싱크로 성공 후 상대 필드 카드 1장 파괴
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
-		if #dg>0 then
-			Duel.Destroy(dg,REASON_EFFECT)
-		end
+	local sc=sg:Select(tp,1,1,nil):GetFirst()
+	if not sc then return end
+
+	Duel.SynchroSummon(tp,sc,nil,mg)
+
+	-- 싱크로 성공 후 파괴
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		Duel.Destroy(dg,REASON_EFFECT)
 	end
 end
 
--- ■ ② 다음 턴 스탠바이 페이즈에 패로 되돌림
+-- ■ ② 다음 턴 회수용 플래그
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
 end
 
+-- ■ ② 다음 턴 스탠바이 시 회수
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and e:GetHandler():GetFlagEffect(id)~=0
+	return Duel.GetTurnPlayer()==tp and e:GetHandler():GetFlagEffect(id)>0
 end
-
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
