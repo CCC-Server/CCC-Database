f39c7d5052959e37faf24b6906c28088a7a6ea6d
badinamchin <mukai123@naver.com>
2025-05-18 14:02:05 +0900

    Add files via upload

---
+++ script/c128101152.lua
@@ -0,0 +1,121 @@
+-- A・O・J 디텍터블 암즈
+local s,id=GetID()
+function s.initial_effect(c)
+	-- 싱크로 소환 조건
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+
+	-- ① 자신/상대 턴에 몬스터 제외 + 빛일 경우 엑덱 확인/제외
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+
+	-- ② 필드 효과 무효 + 덱에서 A.O.J 특수 소환
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+
+	-- ③ 파괴 시 묘지에서 기계족 특수 소환
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.spcon3)
+	e3:SetTarget(s.sptg3)
+	e3:SetOperation(s.spop3)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_ALLY_OF_JUSTICE}
+
+-- ■ ① 필드 몬스터 1장 제외 → 빛일 경우 엑덱 확인/제외
+function s.rmfilter(c)
+	return c:IsMonster() and c:IsAbleToRemove()
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,PLAYER_ALL,LOCATION_MZONE)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	if #g==0 then return end
+	local rc=g:GetFirst()
+	local attr = rc:IsAttribute(ATTRIBUTE_LIGHT)
+	if Duel.Remove(rc,POS_FACEUP,REASON_EFFECT)~=0 and attr then
+		local edg=Duel.GetFieldGroup(1-tp,LOCATION_EXTRA,0)
+		Duel.ConfirmCards(tp,edg)
+		local rg=edg:Filter(Card.IsAbleToRemove,nil)
+		if #rg>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=rg:Select(tp,1,1,nil)
+			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		end
+		Duel.ShuffleExtra(1-tp)
+	end
+end
+
+-- ■ ② 필드 효과 발동 시 → 무효 + 덱에서 A.O.J 특수 소환
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActivated() and re:GetHandler():IsOnField() and Duel.IsChainDisablable(ev)
+end
+function s.aojfilter(c,e,tp)
+	return c:IsSetCard(SET_ALLY_OF_JUSTICE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.aojfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.aojfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+
+-- ■ ③ 파괴되었을 때 묘지에서 기계족 특수 소환
+function s.spcon3(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_BATTLE+REASON_EFFECT)
+end
+function s.spfilter3(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(s.spfilter3,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop3(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter3,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
---
+++ script/c128101153.lua
@@ -0,0 +1,74 @@
+-- A・O・J 슈퍼 마인드
+local s,id=GetID()
+function s.initial_effect(c)
+	-- ① 덱에서 A.O.J 몬스터 서치 + 기계족 외 특소 제한
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.tg1)
+	e1:SetOperation(s.op1)
+	c:RegisterEffect(e1)
+
+	-- ② 묘지에서 기계족 릴리스로 이 카드 회수
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.tg2)
+	e2:SetOperation(s.op2)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_ALLY_OF_JUSTICE}
+
+-- ■ ①: 서치 + 엑덱 기계족 외 특소 제한
+function s.filter1(c)
+	return c:IsSetCard(SET_ALLY_OF_JUSTICE) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+	-- 특수 소환 제한: 기계족 외 엑스트라 덱 불가
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.splimit(e,c)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsRace(RACE_MACHINE)
+end
+
+-- ■ ②: 묘지에서 발동 → 기계족 릴리스 후 패로 회수
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_MACHINE) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_MACHINE)
+	Duel.Release(g,REASON_COST)
+end
+function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,c)
+	end
+end
