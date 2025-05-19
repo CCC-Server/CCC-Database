a9b0aab8efb599479ae812adb47bb93498a8a9e6
badinamchin <mukai123@naver.com>
2025-05-19 21:42:14 +0900

    Update c128101164.lua

--- script/c128101164.lua
+++ script/c128101164.lua
@@ -1,4 +1,4 @@
---RR-섀도우 러치어 (가칭)
+--RR-섀도우 러치어
 local s,id=GetID()
 function s.initial_effect(c)
 	-- ① 덱에서 RR 묘지보내고 특수 소환
@@ -13,15 +13,12 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	-- ② 소재 상태에서 효과 부여
+	-- ② 엑시즈 소재로 사용되었을 때 엑시즈 몬스터에게 효과 부여
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_XYZ_MATERIAL)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_OVERLAY)
-	e2:SetCondition(s.matcon)
-	e2:SetOperation(s.matop)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return r==REASON_XYZ end)
+	e2:SetOperation(s.materialcheck)
 	c:RegisterEffect(e2)
 end
 
@@ -41,42 +38,43 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) > 0 then
-		-- 어둠 속성 이외 특수 소환 금지
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- 어둠 속성 이외 특수 소환 제한
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 		e1:SetTargetRange(1,0)
-		e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsAttribute(ATTRIBUTE_DARK) end)
+		e1:SetTarget(function(_,sc) return sc:IsLocation(LOCATION_EXTRA) and not sc:IsAttribute(ATTRIBUTE_DARK) end)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
 
--- ② 효과 부여 조건: RR 엑시즈 몬스터가 엑시즈 소환되었을 때
-function s.matcon(e)
-	local c=e:GetHandler():GetOverlayTarget()
-	return c and c:IsSetCard(0xba) and c:IsType(TYPE_XYZ)
-end
-function s.matop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler():GetOverlayTarget()
-	if not c then return end
+-- ② 엑시즈 소재로 쓰였을 때 효과 부여
+function s.materialcheck(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if not rc or not rc:IsSummonType(SUMMON_TYPE_XYZ) then return end
+	if not rc:IsSetCard(0xba) then return end
 
-	-- 새로운 효과 부여
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O)
+	-- 엑시즈 몬스터에게 발동 효과 부여
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) end)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
+		return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+	end)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	c:RegisterEffect(e1,true)
+	rc:RegisterEffect(e1,true)
 end
 
--- RUM 카드 세트 조건 및 동작
+-- RUM 카드 필터
 function s.rumfilter(c)
 	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x95) and c:IsSSetable()
 end
@@ -88,7 +86,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.rumfilter,tp,LOCATION_DECK,0,1,1,nil)
 	local tc=g:GetFirst()
 	if tc and Duel.SSet(tp,tc)>0 and tc:IsType(TYPE_QUICKPLAY) then
-		-- 세트한 속공 마법을 세트 턴에 발동 가능하게
+		-- 세트 턴 발동 가능하게 함
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
