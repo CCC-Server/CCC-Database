92eb7e2e42326823a5cb80e48533c6035f7d310b
badinamchin <mukai123@naver.com>
2025-05-21 22:56:29 +0900

    Update c128101167.lua

--- script/c128101167.lua
+++ script/c128101167.lua
@@ -1,12 +1,13 @@
 --어보미네이션 지원병
 local s,id=GetID()
 function s.initial_effect(c)
-	--1: 특수 소환 (기계족 몬스터 효과 발동 시)
+	-- 1: 프리체인 특수 소환 (기계족 효과 발동한 턴)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_CHAINING)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
@@ -14,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	--2: 서치 효과 (소환 성공 시)
+	-- 2: 서치 효과 (소환 성공 시)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -28,7 +29,7 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 
-	--3: 다음 스탠바이 페이즈에 패로 되돌리기
+	-- 3: 다음 스탠바이 페이즈에 패로 되돌리기
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
@@ -39,11 +40,23 @@ function s.initial_effect(c)
 	e4:SetTarget(s.rettg)
 	e4:SetOperation(s.retop)
 	c:RegisterEffect(e4)
+
+	-- 턴 동안 기계족 몬스터 효과 발동 기록용
+	local ge1=Effect.CreateEffect(c)
+	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	ge1:SetCode(EVENT_CHAINING)
+	ge1:SetOperation(s.regop)
+	Duel.RegisterEffect(ge1,0)
 end
 
--- 기계족 몬스터가 효과 발동한 턴
+-- 🛠 1: 프리체인 특수 소환 조건 (턴 중 기계족 효과 발동했는지)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if re:GetHandler():IsRace(RACE_MACHINE) and re:IsActivated() then
+		Duel.RegisterFlagEffect(rp,id,RESET_PHASE+PHASE_END,0,1)
+	end
+end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:GetHandler():IsRace(RACE_MACHINE) and re:IsActivated()
+	return Duel.GetFlagEffect(tp,id)>0
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
@@ -57,7 +70,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- 덱에서 어보미네이션 카드 1장 서치 (자기 자신 제외)
+-- 🧠 2: 어보미네이션 카드 서치
 function s.thfilter(c)
 	return c:IsSetCard(0xc42) and not c:IsCode(id) and c:IsAbleToHand()
 end
@@ -74,7 +87,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- 다음 스탠바이 페이즈에 패로
+-- 🔁 3: 다음 스탠바이 페이즈에 패로 되돌리기
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
