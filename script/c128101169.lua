d252d2884696a0bd520eeb018714acfde78c805a
badinamchin <mukai123@naver.com>
2025-05-22 13:48:43 +0900

    어보미네이션 테네브레 수정

--- script/c128101169.lua
+++ script/c128101169.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_MAIN_END,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetHintTiming(TIMING_MAIN_END,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon1)
@@ -16,16 +16,18 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop1)
 	c:RegisterEffect(e1)
 
-	--2: 소환 성공 시 어보미네이션 마/함 서치
+	--2: 소환 성공시 어보미네이션 마/함 서치 (타이밍 놓치지 않음)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,id+100)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
+
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
@@ -44,16 +46,13 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 
---🔸 1: 메인 페이즈 조건
+-- ① 조건: 메인 페이즈
 function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
 end
-
---🔸 1: 레벨 5 이하 기계족 특소 가능 여부
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_MACHINE) and c:IsLevelBelow(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-
 function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
@@ -63,7 +62,6 @@ function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND)
 end
-
 function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
@@ -73,23 +71,24 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,Group.FromCards(tc,c))
 		if Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)
 			and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-			Duel.SpecialSummonComplete()
-			-- 🔒 비기계족 엑스트라 덱 특수 소환 제한
 			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
-			e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-			e1:SetTargetRange(1,0)
-			e1:SetTarget(function(_,c)
-				return c:IsLocation(LOCATION_EXTRA) and not c:IsRace(RACE_MACHINE)
-			end)
-			e1:SetReset(RESET_PHASE+PHASE_END)
-			Duel.RegisterEffect(e1,tp)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+			e1:SetValue(s.limitmat)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			c:RegisterEffect(e1,true)
+			local e2=e1:Clone()
+			tc:RegisterEffect(e2,true)
+			Duel.SpecialSummonComplete()
 		end
 	end
 end
+function s.limitmat(e,c)
+	return not c:IsRace(RACE_MACHINE) or not c:IsType(TYPE_SYNCHRO)
+end
 
---🔸 2: 덱에서 어보미네이션 마/함 서치
+-- ② 어보미네이션 마/함 서치
 function s.thfilter(c)
 	return c:IsSetCard(0xc42) and (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and c:IsAbleToHand()
 end
@@ -106,7 +105,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---🔸 3: 상대 턴에 싱크로 소환
+-- ③ 상대 턴에 싱크로 소환
 function s.syncon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
 end
