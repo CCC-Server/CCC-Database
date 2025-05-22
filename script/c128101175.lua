2dd684637c3b6f2d57850c07af20962aac09cf9a
badinamchin <mukai123@naver.com>
2025-05-22 11:18:37 +0900

    Update c128101175.lua

--- script/c128101175.lua
+++ script/c128101175.lua
@@ -1,4 +1,4 @@
---어보미네이션 서지 배리어
+--어보미네이션 서지 레조넌스 (지속 함정)
 local s,id=GetID()
 function s.initial_effect(c)
 	--① 카드 발동 (프리체인)
@@ -7,63 +7,63 @@ function s.initial_effect(c)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 
-	--①: 어보미넌스 싱크로 소환시 체인 봉쇄
+	--②: 어보미네이션 특수 소환 시 상대 필드 파괴 (1턴 1회)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_SZONE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,1)
-	e1:SetCondition(s.chainlimit_con)
-	e1:SetValue(s.chainlimit_val)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	--②: 어보미네이션 몬스터 특수 소환 시 파괴 (1턴 1회)
+	--①: 어보미네이션 싱크로 소환 시 체인 봉쇄 (1턴 지속 효과)
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.descon)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetCondition(s.chaincon)
+	e2:SetValue(s.chainlimit)
 	c:RegisterEffect(e2)
 end
 
-----------------------------
--- ① 어보미넌스 싱크로 체인 봉쇄
-----------------------------
-function s.chainlimit_con(e)
-	local tp=e:GetHandlerPlayer()
-	local ph=Duel.GetCurrentPhase()
-	return Duel.GetCurrentChain()==0 and ph==PHASE_MAIN1 or ph==PHASE_MAIN2
-end
-function s.chainlimit_val(e,re,tp)
-	local rc=re:GetHandler()
-	return rc:IsType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP) and Duel.GetFlagEffect(tp,id)==1
+--②: 어보미네이션 특수 소환 확인
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(function(c) return c:IsSetCard(0xc42) and c:IsControler(tp) end,1,nil)
 end
 
-----------------------------
--- ② 어보미네이션 특수 소환 시 파괴
-----------------------------
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(function(c) return c:IsControler(tp) and c:IsSetCard(0xc42) end,1,nil)
-end
+--②: 파괴 대상
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=1
-	if Duel.IsExistingMatchingCard(function(c) return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0xc42) end,tp,LOCATION_MZONE,0,1,nil) then
+	if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_SYNCHRO) then
 		ct=2
 	end
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,ct,nil)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,ct,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
+
+--②: 파괴 실행
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
+end
+
+--①: 어보미네이션 싱크로 소환 시 체인 봉쇄
+function s.chaincon(e)
+	local ph=Duel.GetCurrentPhase()
+	return Duel.GetTurnPlayer()~=e:GetHandlerPlayer() and ph==PHASE_MAIN1 or ph==PHASE_MAIN2
+end
+function s.chainlimit(e,re,tp)
+	local rc=re:GetHandler()
+	return rc:IsType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP) and re:IsActiveType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP)
 end
