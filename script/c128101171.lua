0240af1c783ff35d24288e227e30d52471eba8b9
badinamchin <mukai123@naver.com>
2025-05-22 11:51:55 +0900

    Update c128101171.lua

--- script/c128101171.lua
+++ script/c128101171.lua
@@ -1,33 +1,34 @@
 --어보미네이션 인노미나툼
 local s,id=GetID()
 function s.initial_effect(c)
-	--1: 상대가 몬스터 특소한 턴에 패에서 특소
+	--1: 기계족 이외의 몬스터 효과 발동된 턴에 패에서 특수 소환 + 덤핑
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e1:SetCondition(s.spcon1)
 	e1:SetTarget(s.sptg1)
 	e1:SetOperation(s.spop1)
 	c:RegisterEffect(e1)
 
-	--2: 상대 몬스터 효과 발동 시 덱특소 + 싱크로 소환
+	--2: 상대 턴 싱크로 소환
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_CHAINING)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+100)
-	e2:SetCondition(s.spcon2)
-	e2:SetTarget(s.sptg2)
-	e2:SetOperation(s.spop2)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCondition(function(e,tp) return Duel.GetTurnPlayer()~=tp end)
+	e2:SetTarget(s.syntg2)
+	e2:SetOperation(s.synop2)
 	c:RegisterEffect(e2)
 
-	--3: 묘지에서 자가 부활 (턴 종료시)
+	--3: 묘지에서 자가 부활
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -39,64 +40,78 @@ function s.initial_effect(c)
 	e3:SetTarget(s.sptg3)
 	e3:SetOperation(s.spop3)
 	c:RegisterEffect(e3)
+
+	-- 턴 중 기계족 이외 몬스터 효과 발동 감지
+	if not s.global_check then
+		s.global_check = true
+		local ge = Effect.CreateEffect(c)
+		ge:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge:SetCode(EVENT_CHAINING)
+		ge:SetOperation(function(_,_,_,_,_,re,rp)
+			local rc = re:GetHandler()
+			if re:IsActiveType(TYPE_MONSTER) and not rc:IsRace(RACE_MACHINE) then
+				Duel.RegisterFlagEffect(rp,id,RESET_PHASE+PHASE_END,0,1)
+			end
+		end)
+		Duel.RegisterEffect(ge,0)
+	end
 end
 
--- 효과 ①: 상대가 몬스터 특수 소환한 턴에 발동
-function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp or Duel.GetActivityCount(1-tp,ACTIVITY_SPSUMMON)>0
+-- ① 조건: 기계족 이외 몬스터 효과 발동된 턴
+function s.spcon1(e,tp)
+	return Duel.GetFlagEffect(tp,id)>0
 end
+
+function s.tgfilter(c)
+	return c:IsSetCard(0xc42) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
+
 function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
+
 function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
-
--- 효과 ②: 상대 몬스터 효과 발동 시 반응 → 특소 + 싱크로
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return ep==1-tp and re:IsActiveType(TYPE_MONSTER) and re:IsActivated()
-end
-function s.spfilter2(c,e,tp)
-	return c:IsSetCard(0xc42) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.synfilter(c,mg)
-	return c:IsRace(RACE_MACHINE) and c:IsSynchroSummonable(nil,mg)
-end
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-		Duel.BreakEffect()
-		local sg=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg)
-		if #sg>0 then
-			Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoGrave(g,REASON_EFFECT)
 		end
 	end
 end
 
--- 효과 ③: 이 턴에 묘지로 간 경우, 엔드 페이즈에 부활
-function s.spcon3(e,tp,eg,ep,ev,re,r,rp)
+-- ② 상대 턴 싱크로
+function s.syntg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil) end
+end
+
+function s.synop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,1,nil,nil)
+	if #g>0 then
+		Duel.SynchroSummon(tp,g:GetFirst(),nil)
+	end
+end
+
+-- ③ 엔드페이즈 부활
+function s.spcon3(e,tp)
 	return e:GetHandler():GetTurnID()==Duel.GetTurnCount()
 end
+
 function s.sptg3(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
+
 function s.spop3(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
