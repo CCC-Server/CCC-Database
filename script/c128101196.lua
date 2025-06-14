dab55337aedf789a89557b68c203a584050d3d1c
badinamchin <mukai123@naver.com>
2025-06-15 00:23:54 +0900

    오류수정

--- script/c128101196.lua
+++ script/c128101196.lua
@@ -1,54 +1,55 @@
--- 헤르모스를 따르는 파수꾼
+-- 헤르모스의 융합 몬스터 (가칭)
 local s,id=GetID()
 s.listed_names={46232525} -- "헤르모스의 발톱"
 
 function s.initial_effect(c)
-	-- 특수 소환 제한: "헤르모스의 발톱"의 효과로만 특수 소환 가능
+	c:EnableReviveLimit()
+
+	-- 특수 소환 제한: "헤르모스의 발톱" 효과로만
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetValue(s.splimit)
 	c:RegisterEffect(e0)
 
-	-- ① 특수 소환 성공시: 덱/묘지에서 헤르모스 관련 몬스터 서치
+	-- ①: 특수 소환 성공 시 서치
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 
-	-- ② 자신 / 상대 턴: 이 카드를 장착 + 상대 몬스터 무효화
+	-- ②: 장착 + 파괴
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_EQUIP+CATEGORY_DISABLE)
+	e2:SetCategory(CATEGORY_EQUIP+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_END_PHASE)
 	e2:SetCountLimit(1,id+100)
 	e2:SetTarget(s.eqtg)
 	e2:SetOperation(s.eqop)
 	c:RegisterEffect(e2)
 end
 
--- 특수 소환 제한
+-- 특수 소환 제한: "헤르모스의 발톱"의 효과로만
 function s.splimit(e,se,sp,st)
-	return se and se:GetHandler():IsCode(46232525)
+	return se:GetHandler():IsCode(46232525)
 end
 
--- ① 덱/묘지에서 서치
+-- ① 덱/묘지에서 "헤르모스의 발톱" 카드명이 쓰여진 몬스터 1장 서치
 function s.thfilter(c)
-	return (c:IsCode(46232525) or c:ListsCode(46232525)) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and c:ListsCode(46232525)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
@@ -60,56 +61,42 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- ② 장착 후 상대 몬스터 효과 무효화
-function s.eqfilter(c)
-	return c:IsFaceup() and c:IsControler(c:GetControler())
-end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+-- ② 자신 몬스터 대상 → 이 카드를 장착 → 상대 필드 카드 파괴
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) end
 	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-			and Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
-			and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
+		return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil)
+			and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,1-tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_ONFIELD)
 end
-
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) then return end
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	if not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 
-	if Duel.Equip(tp,c,tc,false) then
-		-- 장착 제한
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EQUIP_LIMIT)
-		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(function(e,tc2) return tc2==tc end)
-		c:RegisterEffect(e1)
+	-- 장착 처리
+	if not Duel.Equip(tp,c,tc) then return end
 
-		-- 상대 몬스터 무효화
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-		local target=g:GetFirst()
-		if target then
-			Duel.NegateRelatedChain(target,RESET_TURN_SET)
+	-- 장착 제한: tc에게만 장착
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(function(e,c) return c==tc end)
+	c:RegisterEffect(e1)
 
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_DISABLE)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			target:RegisterEffect(e2)
-
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_DISABLE_EFFECT)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			target:RegisterEffect(e3)
-		end
+	-- 상대 필드 카드 파괴
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		Duel.Destroy(dg,REASON_EFFECT)
 	end
 end
