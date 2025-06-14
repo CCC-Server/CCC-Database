4c7775b0ef3559f0c51af174e249d463bce1a7a5
ele093 <50264650+ele093@users.noreply.github.com>
2025-06-15 01:38:24 +0900

    서나래님 카드 수정
    스완송 힘멜

--- script/c124131289.lua
+++ script/c124131289.lua
@@ -4,86 +4,78 @@ function s.initial_effect(c)
     --Xyz Summon (Water Level 5 x3)
     Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),5,3)
     c:EnableReviveLimit()
-
-    --①: 자신 필드의 물 속성 몬스터 ATK/DEF +300
+    -- E1: WATER 몬스터 ATK/DEF +300
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetRange(LOCATION_MZONE)
     e1:SetTargetRange(LOCATION_MZONE,0)
-    e1:SetTarget(s.atktg)
+    e1:SetTarget(s.statfilter)
     e1:SetValue(300)
     c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e2)
+    local e1b=e1:Clone()
+    e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e1b)
 
-    -- New Effect ② implementation
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1)) -- This will be the string ID for the effect description
-    e3:SetCategory(CATEGORY_DISABLE) -- This effect negates, so it fits CATEGORY_DISABLE
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O) -- A field effect that grants a Quick Effect
-    e3:SetCode(EVENT_FREE_CHAIN) -- Allows activation during any open game state (Quick Effect)
-    e3:SetRange(LOCATION_MZONE) -- The effect originates from this card on the monster zone
-    e3:SetCountLimit(1) -- Once per turn
-    e3:SetCondition(s.has_lvl2_water_material) -- The condition we discussed: must have a Level 2 WATER Xyz material
-    e3:SetCost(s.negate_cost) -- Function for detaching material
-    e3:SetTarget(s.negate_target) -- Function for targeting opponent's monster
-    e3:SetOperation(s.negate_operation) -- Function for negating the target's effects
-    c:RegisterEffect(e3)
+    -- E2: 조건부 속공 무효 효과
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+    e2:SetCountLimit(1)
+    e2:SetCondition(s.negcon)
+    e2:SetCost(s.negcost)
+    e2:SetTarget(s.negtg)
+    e2:SetOperation(s.negop)
+    c:RegisterEffect(e2)
 end
 
--- ①: 물 속성 몬스터만 공격/수비력 상승 대상
-function s.atktg(e,c)
+-- E1: 필드의 물속성 몬스터만 적용
+function s.statfilter(e,c)
     return c:IsAttribute(ATTRIBUTE_WATER)
 end
 
--- Helper function for Effect ②'s condition
-function s.is_lvl2_water(c)
+-- E2 조건: 이 카드가 레벨 2 WATER 몬스터를 소재로 가지고 있는가?
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetOverlayGroup():IsExists(s.ovfilter,1,nil)
+end
+function s.ovfilter(c)
     return c:IsAttribute(ATTRIBUTE_WATER) and c:IsLevel(2)
 end
 
--- Condition for Effect ② (the effect itself becomes available if this condition is met)
-function s.has_lvl2_water_material(e,tp,eg,ep,ev,re,r,rp)
-    local c = e:GetHandler()
-    -- Check if any Xyz Material of this card is a Level 2 WATER monster
-    return c:GetOverlayGroup():IsExists(s.is_lvl2_water, 1, nil)
+-- E2 비용: 소재 1개 제거
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 
-function s.negfilter(c)
-    return c:IsFaceup() and c:IsCanBeEffectTarget() and aux.disfilter1(c)
+-- E2 타겟: 상대 필드 몬스터 1장
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and s.disfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.disfilter,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,s.disfilter,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
--- Cost for Effect ② (detaching 1 material)
-function s.negate_cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:CheckRemoveOverlay(tp, 1, REASON_COST) end
-    c:RemoveOverlay(tp, 1, 1, REASON_COST)
-end
-
--- Target for Effect ② (1 opponent's monster)
-function s.negate_target(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingTarget(s.negfilter, tp, 0, LOCATION_MZONE, 1, nil) end
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_NEGATE)
-    local g = Duel.SelectTarget(tp, s.negfilter, tp, 0, LOCATION_MZONE, 1, 1, nil)
-    Duel.SetTargetCard(g)
+function s.disfilter(c)
+    return c:IsFaceup() and not c:IsDisabled()
 end
 
--- Operation for Effect ② (negating effects)
-function s.negate_operation(e, tp, eg, ep, ev, re, r, rp)
-    local tc = Duel.GetFirstTarget() -- Get the targeted card
-    -- Ensure the card is still face-up and can have its effects disabled
-    if tc and tc:IsFaceup() and tc:IsCanDisableEffect() then
-        -- Negate the effects of the targeted monster
-        local e1 = Effect.CreateEffect(e:GetHandler())
+-- E2 실행: 대상 몬스터의 효과 무효화
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsFaceup() and not tc:IsDisabled() and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_DISABLE_EFFECT) -- Disables monster effects
-        e1:SetReset(RESET_EVENT+RESETS_DISABLE+RESET_PHASE+PHASE_END) -- Negation lasts until the End Phase
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e1)
-        
-        local e2 = Effect.CreateEffect(e:GetHandler())
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DISABLE_TRAPMONSTER) -- Specifically for Trap Monsters whose effects might not be covered by EFFECT_DISABLE_EFFECT
-        e2:SetReset(RESET_EVENT+RESETS_DISABLE+RESET_PHASE+PHASE_END)
+        local e2=e1:Clone()
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
         tc:RegisterEffect(e2)
     end
 end
