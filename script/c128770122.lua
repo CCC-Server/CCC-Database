f29409fe88598bdebbd8854776e2bc50e02aecc4
multitasker2 <iremnant12@gmail.com>
2025-07-19 11:55:07 +0900

    Add files via upload

--- script/c128770122.lua
+++ script/c128770122.lua
@@ -1,7 +1,9 @@
---마요괴귀혼향
 local s,id=GetID()
 function s.initial_effect(c)
-	--①: 발동시 마요괴 마법 세트
+	-- 이 카드명의 카드는 1장밖에 발동할 수 없다
+	c:SetUniqueOnField(1,0,id)
+	
+	-- ①: 이 카드 발동시 마요괴 마법 1장 세트
 	local e0=Effect.CreateEffect(c)
 	e0:SetDescription(aux.Stringid(id,0))
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,7 +12,7 @@ function s.initial_effect(c)
 	e0:SetOperation(s.setop)
 	c:RegisterEffect(e0)
 
-	--②: 메인 페이즈에 패에서 마요괴 특수 소환
+	-- ②: 패에서 마요괴 몬스터 1장 특수 소환
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -22,43 +24,38 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	--③: 상대 메인 페이즈에 마요괴 싱크로 소환
+	-- ③: 마요괴 싱크로 몬스터가 싱크로 소환되었을 경우 추가 일반 소환
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCountLimit(1,{id,2})
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e2:SetCondition(function(_,tp) return Duel.IsMainPhase() and Duel.GetTurnPlayer()~=tp end)
-	e2:SetTarget(s.syntg)
-	e2:SetOperation(s.synop)
+	e2:SetCondition(s.nscon)
+	e2:SetOperation(s.nsop)
 	c:RegisterEffect(e2)
 end
 
---①: 마요괴 마법 세트
+-- ① 마요괴 마법 세트
 function s.setfilter(c)
 	return c:IsSetCard(0x121) and c:IsType(TYPE_SPELL) and c:IsSSetable()
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SSet(tp,g:GetFirst())
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
 
---②: 패에서 마요괴 몬스터 특수 소환
+-- ② 패에서 마요괴 몬스터 특수 소환
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x121) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -70,25 +67,27 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---③: 상대 메인 페이즈에 마요괴 싱크로 소환
-function s.synfilter(c)
-	return c:IsSetCard(0x121) and c:IsType(TYPE_SYNCHRO)
-end
-function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_TUNER)
-			and Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil)
+-- ③ 마요괴 싱크로 몬스터가 싱크로 소환되었을 경우
+function s.nscon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsSummonType(SUMMON_TYPE_SYNCHRO)
+			and tc:IsSetCard(0x121)
+			and tc:IsType(TYPE_SYNCHRO)
+			and tc:IsControler(tp) then
+			return true
+		end
+		tc=eg:GetNext()
 	end
+	return false
 end
-function s.synop(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	local sg=Duel.GetMatchingGroup(s.synfilter,tp,LOCATION_EXTRA,0,nil)
-	if #sg==0 or #mg==0 then return end
-
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc=sg:Select(tp,1,1,nil):GetFirst()
-	if sc then
-		Duel.SynchroSummon(tp,sc,nil,mg)
-	end
+function s.nsop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_ZOMBIE))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
-
