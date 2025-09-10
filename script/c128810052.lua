c92a7da09e857dd22c027aaa4156ed700c2b41a9
OdinStrange <Seowon3759@gmail.com>
2025-09-10 17:23:44 +0900

    수정

--- script/c128810052.lua
+++ script/c128810052.lua
@@ -1,8 +1,8 @@
---올마이티 셀레스티얼 타이탄-창조자 이터니티
+--올마이티 셀레스티얼 타이탄-이터니티 미라클
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Synchro.AddProcedure(c,nil,2,2,Synchro.NonTunerEx(s.matfilter),1,1)
+	Synchro.AddProcedure(c,nil,3,3,Synchro.NonTunerEx(s.matfilter),1,1)
 	c:AddMustBeSynchroSummoned()
 	Pendulum.AddProcedure(c,false)
 	--Cannot be destroyed by effects
@@ -14,19 +14,23 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
 	--Change a monster effect activated by the opponent
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,id)
-	e4:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp and re:IsMonsterEffect() end)
-	e4:SetCost(s.cost)
-	e4:SetCondition(s.chcon)
-	e4:SetTarget(s.chngtg)
-	e4:SetOperation(s.chngop)
-	c:RegisterEffect(e4)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp and re:IsMonsterEffect() end)
+	e3:SetCost(s.cost)
+	e3:SetCondition(s.chcon)
+	e3:SetTarget(s.chngtg)
+	e3:SetOperation(s.chngop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0xc02}
 s.synchro_nt_required=1
@@ -34,9 +38,16 @@ function s.matfilter(c,val,scard,sumtype,tp)
 	return c:IsRace(RACE_FAIRY,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
 end
 
+function s.costfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xc02) and c:IsType(TYPE_PENDULUM)
+end
+
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,500) end
-	Duel.PayLPCost(tp,1000)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_EXTRA,0,nil)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 and #g>0 and c:IsAbleToRemoveAsCost() end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.TRUE,1,tp,HINTMSG_REMOVE)
+	Duel.Remove(sg,POS_FACEUP,REASON_COST)
 end
 function s.fairyfilter(c)
 	return c:IsRace(RACE_FAIRY) and c:IsMonster() and (c:IsFaceup() or not c:IsOnField())
