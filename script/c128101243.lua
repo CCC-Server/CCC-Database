6b3cbbb620dbe907dc480a85542669596944b8cb
badinamchin <mukai123@naver.com>
2025-09-16 13:30:03 +0900

    오류수정

--- script/c128101243.lua
+++ script/c128101243.lua
@@ -4,10 +4,12 @@ function s.initial_effect(c)
   --① 이 카드명의 카드는 1턴에 1장만 발동 가능 + 발동시 처리
   local e0=Effect.CreateEffect(c)
   e0:SetDescription(aux.Stringid(id,0))
+  -- (카테고리는 굳이 안 붙임: 발동 처리에서 '배치'만 하므로 검색/회수 카테고리 아님)
   e0:SetType(EFFECT_TYPE_ACTIVATE)
-  e0:SetCode(EVENT_FREE_CHAIN)                    -- 패에서 자유롭게 발동
-  e0:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)   -- 동일명 1턴 1회 '발동' 제한
-  e0:SetOperation(s.actop)                        -- 발동시 처리
+  e0:SetCode(EVENT_FREE_CHAIN)
+  -- ★ OATH 형식으로 동일명 발동 1턴 1회 (Pressured Planet과 동일)
+  e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+  e0:SetOperation(s.actop)
   c:RegisterEffect(e0)
 
   --② 1턴에 1번: 필드존 카드 1장 GY → 덱에서 "홀로판타지" 몬스터 1장 서치
@@ -16,31 +18,31 @@ function s.initial_effect(c)
   e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
   e1:SetType(EFFECT_TYPE_IGNITION)
   e1:SetRange(LOCATION_SZONE)
-  e1:SetCountLimit(1,{id,1})                      -- 카드명 하드 OPT
+  e1:SetCountLimit(1,{id,1})  -- 카드명 하드 OPT
   e1:SetTarget(s.thtg)
   e1:SetOperation(s.thop)
   c:RegisterEffect(e1)
 end
 s.listed_series={0xc44}
 
--- ① 발동시: 덱/묘지의 "홀로판타지" 필드마법 1장을 필드존에 '놓을 수 있다'
+-- ① 발동시: 덱/묘지의 "홀로판타지" 필드 마법 1장을 '배치'할 수 있다
 function s.fspellfilter(c)
   return c:IsSetCard(0xc44) and c:IsType(TYPE_FIELD)
 end
 function s.actop(e,tp,eg,ep,ev,re,r,rp)
   local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.fspellfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
   if #g==0 then return end
-  if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+  if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then  -- "배치할 필드 마법을 고르겠습니까?"
     Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3)) -- "필드 마법을 선택"
     local tc=g:Select(tp,1,1,nil):GetFirst()
     if tc and not tc:IsForbidden() then
-      -- 기존 필드존 처리(룰에 따름) 후, 앞면으로 '배치'
+      -- 기존 필드존 처리(룰에 따름) 후 앞면으로 '배치'
       Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
     end
   end
 end
 
--- ② 1턴 1번: FZONE 1장 GY → 덱에서 "홀로판타지" 몬스터 1장 서치
+-- ② 1턴 1번: 필드존 1장 GY → 덱에서 "홀로판타지" 몬스터 1장 서치
 function s.fztgfilter(c) return c:IsLocation(LOCATION_FZONE) and c:IsAbleToGrave() end
 function s.thfilter(c)   return c:IsSetCard(0xc44) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
