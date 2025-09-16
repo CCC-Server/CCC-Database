2e3d5d9ff9fc6dd50ccf2f904781a56438c3e386
badinamchin <mukai123@naver.com>
2025-09-16 12:52:54 +0900

    후레아 영창 수정

--- script/c128101252.lua
+++ script/c128101252.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.negop)
 	c:RegisterEffect(e1)
 
-	--② 묘지에서 제외하고 싱크로 소환 (Quick Effect)
+	--② 묘지에서 제외하고 싱크로 소환 (Quick Effect, HOPT)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -59,6 +59,7 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	end
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local tp=e:GetHandlerPlayer()
 	local g=Duel.GetTargetCards(e)
 	for tc in aux.Next(g) do
 		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
@@ -71,6 +72,8 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 			local e2=e1:Clone()
 			e2:SetCode(EFFECT_DISABLE_EFFECT)
 			tc:RegisterEffect(e2)
+			-- 트랩몬스터도 무효화하려면 아래 줄 추가 가능:
+			-- local e3=e1:Clone(); e3:SetCode(EFFECT_DISABLE_TRAPMONSTER); tc:RegisterEffect(e3)
 			if s.fspellcheck(tp) then
 				Duel.BreakEffect()
 				Duel.Destroy(tc,REASON_EFFECT)
@@ -80,24 +83,40 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 ----------------------------------------
--- ② 묘지 싱크로 소환 (Quick)
+-- ② 묘지 프리체인: "홀로판타지" 포함하여 싱크로 소환
 ----------------------------------------
+-- 엑스트라 후보 필터: 소재 중 '홀로판타지'를 smat로 포함해 소환 가능한가?
+local function can_syn_with_holo(sc,mg)
+	local holo=mg:Filter(Card.IsSetCard,nil,0xc44)
+	for smat in aux.Next(holo) do
+		if sc:IsSynchroSummonable(smat,mg) then return true end
+	end
+	return false
+end
+function s.scfilter(sc,mg) return can_syn_with_holo(sc,mg) end
+
 function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil,mg)
+		return Duel.IsExistingMatchingCard(s.scfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.scop(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil,mg)
-	if #g>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		local sc=sg:GetFirst()
-		if sc then
-			Duel.SynchroSummon(tp,sc,nil,mg)
-		end
-	end
+	if #mg==0 then return end
+	-- 엑스트라에서 '홀로판타지'를 smat로 포함해 소환 가능한 싱크로들 수집
+	local g=Duel.GetMatchingGroup(s.scfilter,tp,LOCATION_EXTRA,0,nil,mg)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=g:Select(tp,1,1,nil):GetFirst()
+	if not sc then return end
+	-- smat(반드시 '홀로판타지' 몬스터) 선택
+	local smats=mg:Filter(function(c,scn,matg) return c:IsSetCard(0xc44) and scn:IsSynchroSummonable(c,matg) end,nil,sc,mg)
+	if #smats==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2)) -- "소재로 포함할 '홀로판타지'를 선택"
+	local smat=smats:Select(tp,1,1,nil):GetFirst()
+	if not smat then return end
+	-- 싱크로 소환 실행 (자신 필드 몬스터만을 소재로, smat 강제 포함)
+	Duel.SynchroSummon(tp,sc,smat,mg)
 end
