8f3d9de7dae9fa387d0296e57b8a4a687fe85c9d
badinamchin <mukai123@naver.com>
2025-12-15 00:33:59 +0900

    오류 수정

--- script/c1281100041.lua
+++ script/c1281100041.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetTargetRange(POS_FACEUP_ATTACK,1) 
+	e1:SetTargetRange(POS_FACEUP_ATTACK,1) -- 기본값: 상대 필드 (sptg에서 변경됨)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -48,11 +48,17 @@ s.listed_series={0xd3, 0xc82}
 -- ①번 효과
 function s.relfilter(c,tp,sc)
 	local target_p = c:GetControler()
-	return c:IsReleasable(SCARD_CHECK_SPPROC) and s.kaiju_unique_check(sc, target_p, c) 
-		and sc:IsCanBeSpecialSummoned(nil,0,tp,false,false,POS_FACEUP_ATTACK,target_p)
+	-- 1. 릴리스 가능 여부 (SCARD_CHECK_SPPROC 플래그 사용)
+	-- 2. 대상 필드(target_p)에 파괴수 고유 제약(1장만 존재) 체크
+	-- 3. 대상 필드(target_p)에 몬스터가 나올 공간(MZone)이 있는지 확인 (c가 릴리스되어 사라지는 것 감안)
+	-- 주의: sc:IsCanBeSpecialSummoned()를 여기서 호출하면 무한 루프 발생하므로 제거
+	return c:IsReleasable(SCARD_CHECK_SPPROC) 
+		and s.kaiju_unique_check(sc, target_p, c) 
+		and Duel.GetMZoneCount(target_p, c, tp) > 0
 end
 
 function s.kaiju_unique_check(c, tp, relcard)
+	-- 릴리스할 카드(relcard)를 제외하고 필드에 다른 파괴수가 있는지 확인
 	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,0xd3),tp,LOCATION_MZONE,0,nil)
 	if relcard then g:RemoveCard(relcard) end
 	return #g == 0
@@ -70,8 +76,12 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	if #g>0 then
 		local tc=g:GetFirst()
 		local target_p = tc:GetControler()
+		
+		-- [수정됨] SetSPSummonParam -> SetTargetRange 사용
+		-- 소환될 위치 설정: 0=자신 필드, 1=상대 필드 (SetTargetRange의 두 번째 인자로 제어)
 		local param_p = (target_p==tp) and 0 or 1
-		e:SetSPSummonParam(POS_FACEUP_ATTACK, param_p)
+		e:SetTargetRange(POS_FACEUP_ATTACK, param_p)
+		
 		e:SetLabelObject(tc)
 		return true
 	end
@@ -89,7 +99,7 @@ function s.thfilter(c)
 end
 
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return true end -- 강제 효과
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,e:GetHandler():GetOwner(),LOCATION_DECK)
 end
 
@@ -111,27 +121,22 @@ function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	-- 2. 체인 무효화 가능 여부 확인
 	if not Duel.IsChainNegatable(ev) then return false end
 
-	-- 3. 함정 카드면 무조건 발동 (위치 상관 없음)
-	if re:IsActiveType(TYPE_TRAP) then return true end
+	-- 3. 발동 위치 체크: "패" 또는 "필드"여야 함 (묘지/제외존 효과는 안 막음)
+	local loc = Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	local is_hand_or_field = (loc & (LOCATION_HAND+LOCATION_ONFIELD)) ~= 0
+	if not is_hand_or_field then return false end
 
-	-- 4. 몬스터 효과인 경우: "패 또는 필드"에서 발동했고 AND "기계족이 아닐 경우"에만 발동
-	if re:IsActiveType(TYPE_MONSTER) then
-		local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-		local race=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_RACE)
-		
-		-- 위치가 패(LOCATION_HAND) 또는 필드(LOCATION_ONFIELD) 인지 확인 (비트 연산)
-		local is_hand_or_field = (loc & (LOCATION_HAND+LOCATION_ONFIELD)) ~= 0
-		
-		-- 종족이 기계족이 아닌지 확인 (비트 연산)
-		local is_not_machine = (race & RACE_MACHINE) == 0
-		
-		-- 패/필드에서 발동한 비-기계족 몬스터라면 무효화
-		if is_hand_or_field and is_not_machine then
-			return true
-		end
+	-- 4. 대상 카드 체크: "함정" 이거나 "기계족 이외의 몬스터"여야 함
+	local rc = re:GetHandler()
+	if re:IsActiveType(TYPE_TRAP) then
+		return true -- 함정은 종족 상관없이(사실 종족 없지만) 무효
+	elseif re:IsActiveType(TYPE_MONSTER) then
+		-- 몬스터면 기계족인지 확인
+		local race = Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_RACE)
+		-- 기계족(RACE_MACHINE)이 아니면(==0) 무효
+		return (race & RACE_MACHINE) == 0
 	end
 	
-	-- 그 외(마법, 묘지/제외존 몬스터 효과, 패/필드의 기계족 몬스터 효과)는 무효화하지 않음
 	return false
 end
 
