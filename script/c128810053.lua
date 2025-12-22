38cb44fae2ca8d7ca1e60b5506ae49f3c233bb21
OdinStrange <Seowon3759@gmail.com>
2025-12-22 17:10:47 +0900

    에아 효과 조정

--- script/c128810053.lua
+++ script/c128810053.lua
@@ -16,6 +16,18 @@ function s.initial_effect(c)
 	e1:SetTarget(s.pltg)
 	e1:SetOperation(s.plop)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	-- 비용: 이 카드를 릴리스
+	e2:SetCost(s.relcost)
+	-- 대상: 패의 천사족 튜너
+	e2:SetTarget(s.target)
+	-- 처리: 특수 소환 실행
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e1)
 end
 --천사족밖에 일반 소환 / 특수 소환 불가 맹세 효과
 function s.splimit(e,c)
@@ -62,5 +74,44 @@ function s.plop(e,tp,eg,ep,ev,re,r,rp)
 			end
 			pc1:SetStatus(STATUS_EFFECT_ENABLED,true)
 		end
+	end
+end
+
+-- 필터: 천사족(RACE_FAIRY)이면서 튜너(TYPE_TUNER)이고 특수 소환 가능한 몬스터
+function s.filter(c,e,tp)
+	return c:IsRace(RACE_FAIRY) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.relcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	-- 비용으로 자신을 릴리스하므로, 자신을 포함하여 메인 몬스터 존의 빈 공간을 계산 (GetMZoneCount)
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	-- 효과 처리 시점에 몬스터 존에 자리가 있는지 다시 확인
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
