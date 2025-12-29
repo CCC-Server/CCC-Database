e5df309fa3e63215d44c6fce7d4c9fc1daf647c0
taricrazy <sucle321@gmail.com>
2025-12-29 15:08:14 +0900

    F로그라이크 수정

--- script/c128220110.lua
+++ script/c128220110.lua
@@ -1,42 +1,90 @@
 --F로그라이크 헤드 크러셔
 local s,id=GetID()
 function c128220110.initial_effect(c)
-local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_ANNOUNCE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EVENT_TO_HAND)
-    e1:SetCondition(s.pubcon)
-    e1:SetOperation(s.pubop)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.fustg)
+    e1:SetOperation(s.fusop)
     c:RegisterEffect(e1)
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
-    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e2:SetCondition(s.handcon)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_TO_HAND)
+    e2:SetCondition(s.pubcon)
+    e2:SetOperation(s.pubop)
     c:RegisterEffect(e2)
-	local e3=Fusion.CreateSummonEff({
-		handler=c,
-		fusfilter=aux.FilterBoolFunction(Card.IsRace,RACE_ZOMBIE),
-		matfilter=Fusion.OnFieldMat(Card.IsAbleToGrave),
-		extrafil=s.fextra,
-		stage2=s.stage2
-	})
-	e3:SetCountLimit(1,id)
-	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCode(EVENT_TO_HAND)
-	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCountLimit(1,{id,1})
-	e4:SetCondition(s.thcon)
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
-	c:RegisterEffect(e4)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e3:SetCondition(s.handcon)
+    c:RegisterEffect(e3)
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetCategory(CATEGORY_TOHAND)
+    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCode(EVENT_TO_HAND)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCountLimit(1,{id,1})
+    e4:SetCondition(s.thcon)
+    e4:SetTarget(s.thtg)
+    e4:SetOperation(s.thop)
+    c:RegisterEffect(e4)
+end
+function s.mfilter(c)
+    return c:IsOnField() and c:IsAbleToGrave()
+end
+function s.exfilter(c)
+    return c:IsSetCard(0xc25) and c:IsLocation(LOCATION_DECK) and c:IsAbleToGrave()
+end
+function s.ffilter(c,e,tp,m,f,chkf)
+    return c:IsRace(RACE_ZOMBIE) and (not f or f(c))
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local chkf=tp
+        local mg1=Duel.GetFusionMaterial(tp):Filter(s.mfilter,nil)
+        local mg2=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_DECK,0,nil)
+        local res=Duel.IsExistingMatchingCard(s.ffilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+        if not res and #mg2>0 then
+            for tc in aux.Next(mg2) do
+                mg1:AddCard(tc)
+                res=Duel.IsExistingMatchingCard(s.ffilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+                mg1:RemoveCard(tc)
+                if res then break end
+            end
+        end
+        return res
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local chkf=tp
+    local mg1=Duel.GetFusionMaterial(tp):Filter(s.mfilter,nil)
+    local exg=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_DECK,0,nil)
+    local fcheck=function(tp,sg,fc)
+        return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
+    end
+    local mg=mg1:Clone()
+    mg:Merge(exg)
+    local sg=Duel.GetMatchingGroup(s.ffilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,fcheck,chkf)
+    if #sg>0 then
+        local tg=sg:Select(tp,1,1,nil)
+        local tc=tg:GetFirst()
+        local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
+        tc:SetMaterial(mat)
+        Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+        Duel.BreakEffect()
+        Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+        tc:CompleteProcedure()
+    end
 end
 function s.pubcon(e,tp,eg,ep,ev,re,r,rp)
     return not (bit.band(r,REASON_DRAW)==REASON_DRAW)
@@ -54,31 +102,19 @@ end
 function s.handcon(e)
     return e:GetHandler():GetFlagEffect(id)>0
 end
-function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0xc25),s.fcheck
-end
-function s.fcheck(tp,sg,fc)
-	return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
-end
-function s.stage2(e,tc,tp,sg,chk)
-	if chk==1 and not tc:IsSetCard(0xc25) and sg:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then
-		return false 
-	end
-end
 function s.cfilter(c,tp)
-	return c:IsControler(tp) and not c:IsReason(REASON_DRAW)
+    return c:IsControler(tp) and not c:IsReason(REASON_DRAW)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+    return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToHand() end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+    if chk==0 then return e:GetHandler():IsAbleToHand() end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SendtoHand(c,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,c)
-	end
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
+    end
 end
