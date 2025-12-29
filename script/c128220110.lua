2c1d25e86901881eb0c185436a6077b59f0c364e
taricrazy <sucle321@gmail.com>
2025-12-29 10:04:24 +0900

    F로그라이크 수정

--- script/c128220110.lua
+++ script/c128220110.lua
@@ -1,39 +1,58 @@
 --F로그라이크 헤드 크러셔
 local s,id=GetID()
 function c128220110.initial_effect(c)
-	local e1=Fusion.CreateSummonEff({
+local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_ANNOUNCE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_TO_HAND)
+    e1:SetCondition(s.pubcon)
+    e1:SetOperation(s.pubop)
+    c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCondition(s.handcon)
+    c:RegisterEffect(e2)
+	local e3=Fusion.CreateSummonEff({
 		handler=c,
 		fusfilter=aux.FilterBoolFunction(Card.IsRace,RACE_ZOMBIE),
 		matfilter=Fusion.OnFieldMat(Card.IsAbleToGrave),
 		extrafil=s.fextra,
 		stage2=s.stage2
 	})
-	e1:SetCountLimit(1,id)
-	c:RegisterEffect(e1)
-	
-	-- 패 발동 조건: 자신 필드에 "F로그라이크" 카드가 존재할 경우
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_TRAP_ACT_IN_HAND)
-	e0:SetCondition(s.handcon)
-	c:RegisterEffect(e0)
-
-	-- ②: 묘지 회수 (드로우 이외의 방법으로 패에 넣어졌을 경우)
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_HAND)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
+	e3:SetCountLimit(1,id)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_TO_HAND)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(s.thcon)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+end
+function s.pubcon(e,tp,eg,ep,ev,re,r,rp)
+    return not (bit.band(r,REASON_DRAW)==REASON_DRAW)
+end
+function s.pubop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_PUBLIC)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    c:RegisterEffect(e1)
+    c:RegisterFlagEffect(id, RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END, 0, 1)
 end
 function s.handcon(e)
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xc25),e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+    return e:GetHandler():GetFlagEffect(id)>0
 end
 function s.fextra(e,tp,mg)
 	return Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0xc25),s.fcheck
