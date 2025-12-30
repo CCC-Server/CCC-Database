ba532d69182c01fa7f73f28b803043dd4967cff2
OdinStrange <Seowon3759@gmail.com>
2025-12-30 16:07:59 +0900

    수정

--- script/c128810140.lua
+++ script/c128810140.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_DOUBLE_XYZ_MATERIAL)
 	e1:SetValue(1)
 	e1:SetCountLimit(1,{id,1})
-	e1:SetOperation(function(e,c) return c.minxyzct and c.minxyzct>=3 and c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK) end)
+	e1:SetOperation(function(e,c) return c.minxyzct and c.minxyzct>=3 and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) end)
 	c:RegisterEffect(e1)
 
 	--② 필드 이외에서 묘지로 보내졌을 경우: 덱에서 "헤블론" 마/함 1장 세트
