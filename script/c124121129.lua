72b10b799b97bbefbde247a0b953433b2b5feecb
Linetail <necloth99@gmail.com>
2026-02-22 18:41:56 +0900

    환홍 수정

--- script/c124121129.lua
+++ script/c124121129.lua
@@ -2,12 +2,12 @@
 local s,id=GetID()
 function s.initial_effect(c)
     -- ①: 덱에서 줄투르크 특수 소환 (또는 3장 덤핑)
-    -- 턴 제약 없음
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+    e1:SetCountLimit(1,id) -- [추가됨] ①번 효과 1턴에 1번 제약
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
@@ -31,7 +31,7 @@ function s.initial_effect(c)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_TO_GRAVE)
-    e3:SetCountLimit(1,{id,1})
+    e3:SetCountLimit(1,{id,1}) -- ②번 효과 1턴에 1번 제약
     e3:SetCondition(s.spcon2)
     e3:SetTarget(s.sptg2)
     e3:SetOperation(s.spop2)
@@ -73,7 +73,6 @@ end
 
 -- [① 타겟 처리 (토이 솔저 구조 적용)]
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    -- 토이 솔저의 hasbox처럼 hassurtur 변수로 필드 존재 여부 확인 (마함존 취급 등도 고려해 LOCATION_ONFIELD 사용)
     local hassurtur = Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,s.code_surtur),tp,LOCATION_ONFIELD,0,1,nil)
     
     local b1 = Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_DECK,0,1,nil,e,tp)
@@ -98,16 +97,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     
     if not (b1 or b2) then return end
     
-    -- 두 가지 모두 가능할 때 사용자에게 대신 덤핑할지 질문
     if b1 and b2 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-        b1 = false -- 덤핑을 선택했으므로 특수 소환은 취소
+        b1 = false 
     elseif b2 and not b1 then
-        -- 덤핑만 가능할 경우 그대로 진행
+        -- 덤핑만 가능
     else
-        b2 = false -- 특수 소환을 선택했거나, 애초에 특수 소환만 가능한 경우
+        b2 = false 
     end
     
-    -- 최종 결정된 효과 실행
     if b1 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local g=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp)
@@ -153,7 +150,6 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter2),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp)
     if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
         local tg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_GRAVE,0,nil)
-        -- 3번 스트링: "묘지의 함정 카드를 전부 덱으로 되돌리겠습니까?"
         if #tg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
             Duel.BreakEffect()
             Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
