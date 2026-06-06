d05dea13cf5ff6c6ef873adab9e3adc038c6bdc6
Linetail <168361148+Moonseller99@users.noreply.github.com>
2026-06-06 11:33:53 +0900

    나츄르 수정

--- script/c124121094.lua
+++ script/c124121094.lua
@@ -11,7 +11,6 @@ function s.initial_effect(c)
 	e1:SetTarget(s.tar1)
 	e1:SetOperation(s.op1)
 	c:RegisterEffect(e1)
-	-- 기존의 일반 소환 유발 효과 e2 (EVENT_SUMMON_SUCCESS) 부분 완전 삭제됨
 	
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
@@ -28,28 +27,25 @@ function s.initial_effect(c)
 end
 s.listed_series={0x2a}
 
--- ①번 효과 Condition: 마법 카드의 발동 또는 일반 소환된 몬스터의 효과 발동
+-- ①번 효과 Condition: 일반 소환된 몬스터의 효과 또는 마법 카드가 발동했을 때
 function s.con1(e,tp,eg,ep,ev,re,r,rp)
-	-- 마법 카드의 발동일 때
+	-- 마법 카드가 발동했을 때
 	if re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		return true
 	end
-	-- 일반 소환된 몬스터의 효과 발동일 때
-	if re:IsActiveType(TYPE_MONSTER) then
-		local rc=re:GetHandler()
-		return rc:IsLocation(LOCATION_MZONE) and rc:IsSummonType(SUMMON_TYPE_NORMAL)
-	end
-	return false
+	
+	-- 엘섀도르 세키나가 사양 반영: 일반 소환된 몬스터의 효과가 발동했을 때
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	return loc==LOCATION_MZONE and re:IsMonsterEffect()
+		and (re:GetHandler():GetSummonType()&SUMMON_TYPE_NORMAL)==SUMMON_TYPE_NORMAL
 end
 
--- ①번 효과 Cost: 원본의 패/필드 묘지행 및 체인 플래그 로직 그대로 유지
 function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		return c:IsAbleToGraveAsCost() and Duel.GetFlagEffect(tp,id)==0
 	end
 	Duel.SendtoGrave(c,REASON_COST)
-	-- 동일 체인 발동 불가 처리를 위해 RESET_CHAIN 플래그 등록
 	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
 end
 
@@ -77,12 +73,10 @@ function s.op1(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- ②번 효과 Condition
 function s.con3(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()&(PHASE_MAIN1+PHASE_MAIN2)~=0
 end
 
--- ②번 효과 Cost: 원본의 체인 플래그 체크 로직 그대로 유지 (①번과 id 플래그 공유로 동일 체인 제약 자동 성립)
 function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetFlagEffect(tp,id)==0
