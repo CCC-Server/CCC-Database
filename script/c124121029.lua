9c6dcc17682580a70e5a0b1706300e799def7531
Linetail <168361148+Moonseller99@users.noreply.github.com>
2026-07-06 06:41:59 +0900

    머피 수정

--- script/c124121029.lua
+++ script/c124121029.lua
@@ -1,116 +1,116 @@
---셰터드 섀도우 포에베
+--섀터드 섀도우 블랙 머피
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableReviveLimit()
-	Link.AddProcedure(c,s.pfil1,1,1)
+	-- ①: 자신 / 상대 메인 페이즈에 덱에서 카드를 묘지로 보내고 링크 소환
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_MATERIAL_CHECK)
-	e1:SetValue(s.val1)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE) -- 텍스트에 별도 장소 지정이 없으므로 필드 발동으로 설정
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.con1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.tar1)
+	e1:SetOperation(s.op1)
 	c:RegisterEffect(e1)
+	
+	-- ②: 드로우 이외의 방법으로 패에 넣어졌을 경우 (기존 코드 완벽 유지)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetCountLimit(1,id)
+	e2:SetCode(EVENT_TO_HAND)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(s.con2)
-	e2:SetCost(s.cost2)
 	e2:SetTarget(s.tar2)
 	e2:SetOperation(s.op2)
 	c:RegisterEffect(e2)
-	e2:SetLabelObject(e1)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetCountLimit(1,{id,1})
-	e3:SetCondition(s.con3)
-	e3:SetCost(s.cost3)
-	e3:SetTarget(s.tar3)
-	e3:SetOperation(s.op3)
-	c:RegisterEffect(e3)
 end
-function s.pfil1(c,lc,sumtype,tp)
-	return c:IsLevelBelow(4) and c:IsRace(RACE_WINGEDBEAST,lc,sumtype,tp) and c:IsAttribute(ATTRIBUTE_DARK,lc,sumtype,tp)
+s.listed_names={CARD_POLYMERIZATION,124121029} -- 융합, 섀터드 섀도우 포에베
+
+-- ①번 효과 조건: 자신 / 상대 메인 페이즈
+function s.con1(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
 end
-function s.vfil1(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
+
+-- ①번 효과 코스트: 덱에서 "섀터드 섀도우" 카드나 "융합" 1장을 묘지로 보냄
+function s.cfil1(c)
+	return (c:IsSetCard(0xfa2) or c:IsCode(CARD_POLYMERIZATION)) and c:IsAbleToGraveAsCost()
 end
-function s.val1(e,c)
-	local tp=c:GetControler()
-	local g=c:GetMaterial()
-	e:SetLabel(1)
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfil1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfil1,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
 end
+
+-- ①번 효과 타겟/오퍼레이션 필터: "섀터드 섀도우 포에베"가 현재 링크 소환 가능한가?
+function s.lkfilter(c)
+	return c:IsCode(124121029) and c:IsLinkSummonable(nil)
+end
+function s.tar1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.lkfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+-- ①번 효과 처리: 포에베 링크 소환 실행
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.lkfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.LinkSummon(tp,tc,nil)
+	end
+end
+
+-- =========================================================================
+-- 이하 ②번 효과 관련 함수 (수정 요청에 따라 원본 100% 유지)
+-- =========================================================================
 function s.con2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_LINK) and e:GetLabelObject():GetLabel()==1
+	return not c:IsReason(REASON_DRAW) and Duel.GetCurrentPhase()~=PHASE_DAMAGE 
 end
-function s.cfil2(c)
-	return c:IsAbleToGraveAsCost() and (c:IsCode(24094653) or c:IsCode(48130397))
+function s.tfil2(c)
+	return c:IsCode(24094653) and c:CheckActivateEffect(true,true,false)~=nil
 end
-function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.cfil2,tp,LOCATION_DECK,0,nil)
-	if chk==0 then
-		return #g>0
+function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local tg=e:GetLabelObject():GetTarget()
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
 	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local sg=g:Select(tp,1,1,nil)
-	Duel.SendtoGrave(sg,REASON_COST)
-end
-function s.tfil2(c,tp)
-	return s.vfil1(c,tp) and c:IsAbleToHand()
-end
-function s.tar2(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local g=c:GetMaterial()
 	if chk==0 then
-		return g:IsExists(s.tfil2,1,nil,tp)
+		return Duel.IsExistingTarget(s.tfil2,tp,LOCATION_GRAVE,0,1,nil)
 	end
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-end
-function s.ofil2(c,e,tp)
-	return s.vfil1(c,tp) and c:IsAbleToHand() and c:IsRelateToEffect(e)
+	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e:SetCategory(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.tfil2,tp,LOCATION_GRAVE,0,1,1,nil)
+	local te=g:GetFirst():CheckActivateEffect(true,true,false)
+	Duel.ClearTargetCard()
+	e:SetProperty(te:GetProperty())
+	e:SetLabel(te:GetLabel())
+	e:SetLabelObject(te:GetLabelObject())
+	local tg=te:GetTarget()
+	if tg then
+		tg(e,tp,eg,ep,ev,re,r,rp,1)
+	end
+	te:SetLabel(e:GetLabel())
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+	Duel.ClearOperationInfo(0)
 end
 function s.op2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=c:GetMaterial()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local sg=g:FilterSelect(tp,s.ofil2,1,1,nil,e,tp)
-	if #sg>0 then
-		Duel.SendtoHand(sg,nli,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,sg)
-	end
-end
-function s.nfil3(c)
-	return c:IsSetCard(0xfa2) and c:IsType(TYPE_FUSION) and c:IsFaceup()
-end
-function s.con3(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.nfil3,tp,LOCATION_MZONE,0,1,nil)
-end
-function s.cfil3(c)
-	return c:IsSetCard(0x46) and c:IsType(TYPE_SPELL) and (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsAbleToDeckAsCost()
-end
-function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.cfil3,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
-	if chk==0 then
-		return #g>0
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local sg=g:Select(tp,1,1,nil)
-	Duel.SendtoDeck(sg,nil,1,REASON_COST)
-end
-function s.tar3(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-end
-function s.op3(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	local te=e:GetLabelObject()
+	if te:GetHandler():IsRelateToEffect(e) then
+		e:SetLabel(te:GetLabel())
+		e:SetLabelObject(te:GetLabelObject())
+		local op=te:GetOperation()
+		if op then
+			op(e,tp,eg,ep,ev,re,r,rp)
+		end
+		te:SetLabel(e:GetLabel())
+		te:SetLabelObject(e:GetLabelObject())
 	end
 end
